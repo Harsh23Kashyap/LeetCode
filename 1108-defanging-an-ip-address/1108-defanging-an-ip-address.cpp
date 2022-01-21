dfcfd0edeeb88a6828c18bdd7efaaf24f5cc7a3d
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2022-01-21 02:04:27 -0800

    Time: 0 ms (100.00%), Space: 6 MB (69.03%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string defangIPaddr(string address) 
+    {
+        string s="";
+        for(int i=0;i<address.length();i++)
+        {
+            if(address[i]=='.')
+                s+="[.]";
+            else
+                s+=address[i];
+        }
+        return s;
+        
+    }
+};
