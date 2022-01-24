b692655ed6f8fcd5aaff9cfbc6b87311f0c84015
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2022-01-23 19:59:42 -0800

    Time: 4 ms (57.42%), Space: 7.2 MB (65.72%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,26 +1,49 @@
 class Solution {
 public:
-    vector<int> pass(vector<int>& nums, int curr)
+    // vector<int> pass(vector<int>& nums, int curr)
+    // {
+    //     vector<int> v;
+    //     int ind=0;
+    //     while(curr>0)
+    //     {
+    //         if(curr&1)
+    //             v.push_back(nums[ind]);
+    //         curr=curr>>1;
+    //         ind++;
+    //     }
+    //     return v;
+    // }
+    // vector<vector<int>> subsets(vector<int>& nums) 
+    // {
+    //     vector<vector<int>> sub;
+    //     sub.push_back({});
+    //     for(int i=1;i<(1<<nums.size());i++)
+    //     {
+    //         sub.push_back(pass(nums,i));
+    //     }
+    //     return sub;
+    // }
+    void recursive(vector<vector<int>>& sub, vector<int>& v, int curr, vector<int>& nums)
     {
-        vector<int> v;
-        int ind=0;
-        while(curr>0)
+        if(curr==nums.size())
         {
-            if(curr&1)
-                v.push_back(nums[ind]);
-            curr=curr>>1;
-            ind++;
+            sub.push_back(v);
+            return ;
         }
-        return v;
+        //add
+        v.push_back(nums[curr]);
+        recursive(sub,v,curr+1,nums);
+        //remove
+        v.pop_back();
+        recursive(sub,v,curr+1,nums);
+        return ;
     }
     vector<vector<int>> subsets(vector<int>& nums) 
     {
         vector<vector<int>> sub;
-        sub.push_back({});
-        for(int i=1;i<(1<<nums.size());i++)
-        {
-            sub.push_back(pass(nums,i));
-        }
+        vector<int> v;
+        //sub.push_back({});
+        recursive(sub,v,0,nums);
         return sub;
     }
 };
