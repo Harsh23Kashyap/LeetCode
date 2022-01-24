1a916e9ab4d02637ed0154ff6954a39872a0df5c
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2022-01-24 00:44:38 -0800

    Time: 18 ms (16.54%), Space: 9.5 MB (86.54%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int count(vector<int>& nums, int j)
+    {
+        int c=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if((nums[i]>>j)&1)
+                c++;
+        }
+        cout<<"At position"<<j+1<<" - "<<c<<endl;
+        return c;
+    }
+    int setBit(int n,int i)
+    {
+        int mask=1<<i;
+        int ans=n|mask;
+        return ans;
+    }
+    int singleNumber(vector<int>& nums) 
+    {
+        int num=0;
+        for(int i=0;i<32;i++)
+        {
+            int add=count(nums,i);
+            add=add%3;
+            if(add==1)
+                num=setBit(num,i);
+            
+        }
+        return num;
+    }
+};
