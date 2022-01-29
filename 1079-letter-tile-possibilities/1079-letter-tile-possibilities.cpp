f89b0a1ab0180ef15ccdc6d3a7bd6bb6b8777df4
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2022-01-29 17:46:24 +0530

    Time: 84 ms (37.69%), Space: 15.1 MB (35.92%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    void backtrack(string tiles, int start, int &count)
+    void backtrack(string tiles, int start, set<string>& s)
     {
-        count++;
-        //cout<<tiles<<endl;
+        s.insert(tiles.substr(0,start));
+        cout<<tiles.substr(0,start)<<endl;
         for(int i=start;i<tiles.length();i++)
         {
             if(i != start && tiles[i] == tiles[start]){
                 continue;
             }
             swap(tiles[i], tiles[start]);
-            backtrack(tiles, start+1, count);
+            backtrack(tiles, start+1, s);
+            swap(tiles[i], tiles[start]);
         }
     }
    
     int numTilePossibilities(string tiles) 
     {
+        set<string> s;
         sort(tiles.begin(),tiles.end());
         int count=-1;
         //string t="";
-       backtrack(tiles,0,count);
-        return count;
+        backtrack(tiles,0,s);
+        return s.size()-1;
     }
 };
