62dd4272bc217e992e109212af83c5130bf6fcc4
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2022-02-19 05:53:10 +0530

    Time: 31 ms (39.39%), Space: 17.7 MB (45.44%) - LeetHub

--- 324-wiggle-sort-ii/324-wiggle-sort-ii.cpp
+++ 324-wiggle-sort-ii/324-wiggle-sort-ii.cpp
@@ -2,34 +2,28 @@ class Solution {
 public:
     void wiggleSort(vector<int>& nums) 
     {
-        // for(int i=1;i<nums.size();i+=2)
-        // {
-        //     if(nums[i]<nums[i-1])
-        //         swap(nums[i],nums[i-1]);
-        //     if(i!=nums.size()-1 and nums[i]<nums[i+1])
-        //         swap(nums[i],nums[i+1]);
-        // }
-//         sort(nums.begin(),nums.end());
-        
-//         int n=nums.size();
-//         int k=n-1;
-//         vector<int>ans(n,0);
-//         for(int i=1;i<n;i+=2)
-//             ans[i]=nums[k--];
-//         k=0;
-//         for(int i=0;i<n;i+=2)
-//             ans[i]=nums[k++];
-        
-//         for(int i=0;i<n;i++)
-//             nums[i]=ans[i];
-        vector<int> sorted(nums);
-        sort(sorted.begin(), sorted.end());
-        for (int i=nums.size()-1, j=0, k=i/2+1; i>=0; i--)
+        vector<int> sorted(nums.size());
+        sort(nums.begin(), nums.end());
+        int i=1;
+        int j=nums.size()-1;
+        while(i<nums.size())
         {
-            if(i%2==0)
-                nums[i]=sorted[j++];
-            else
-                nums[i]=sorted[k++];
+            sorted[i]=nums[j];
+            j--;
+            i+=2;
+        }
+        i=0;
+        while(i<nums.size())
+        {
+            sorted[i]=nums[j];
+            j--;
+            i+=2;
+        }
+        
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            nums[i]=sorted[i];
         }
     }
 };
