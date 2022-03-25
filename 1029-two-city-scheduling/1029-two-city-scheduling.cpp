8aba14ebfb51765e304d5d5e684ca5777bffa2d6
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2022-03-25 11:49:05 +0530

    Time: 7 ms (52.53%), Space: 7.9 MB (78.03%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
+    static bool cmp ( vector <int> &a, vector <int> &b )
+    {
+        if( a[0] - a[1]  < b[0] - b[1]) 
+            return true; 
+        return false;
+    }
     int twoCitySchedCost(vector<vector<int>>& costs) 
     {
-        for(int i=0;i<costs.size();i++)
-        {
-            int diff=costs[i][0]-costs[i][1];
-            costs[i].insert(costs[i].begin(),diff);
-        }
+//         for(int i=0;i<costs.size();i++)
+//         {
+//             int diff=costs[i][0]-costs[i][1];
+//             costs[i].insert(costs[i].begin(),diff);
+//         }
         
-        sort(costs.begin(),costs.end());
+        sort(costs.begin(),costs.end(),cmp);
         //reverse(costs.begin(),costs.end());
         
         int sum=0;
@@ -16,10 +22,10 @@ public:
         {
             if(i<costs.size()/2)
             {
-                sum+=costs[i][1];
+                sum+=costs[i][0];
             }
             else
-                sum+=costs[i][2];
+                sum+=costs[i][1];
         }
         
         
