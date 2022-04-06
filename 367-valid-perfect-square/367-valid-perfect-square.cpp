4b5a9d9df8a5b790e0a9935b77504223843662b4
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2022-04-06 18:59:29 +0530

    Time: 1795 ms (6.75%), Space: 6 MB (17.16%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if(num==1) return true;
+        for(long i=1;i<=num/2;i++)
+        {
+            if(i*i==num)return true;
+        }
+        return false;
+    }
+};
