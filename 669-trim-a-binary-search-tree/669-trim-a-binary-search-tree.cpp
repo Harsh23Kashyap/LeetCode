4fe2a17f7ab9e34e19adae720dcdd29358dcac10
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2022-04-15 10:49:41 +0530

    Time: 24 ms (43.03%), Space: 23.9 MB (74.78%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
@@ -19,9 +19,11 @@ public:
             return trimBST(root->right,low,high);
         else if(root->val>high)
              return trimBST(root->left,low,high);
-        
+        else
+        {
         root->left=trimBST(root->left,low,high);
         root->right=trimBST(root->right,low,high);
+        }
         return root;
         
     }
