9a77ac6bdb8d6586034b6be7ad32e5b60ed01ddf
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2022-04-26 16:50:23 +0530

    Time: 16 ms (98.36%), Space: 11.3 MB (54.91%) - LeetHub

---
+++ 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.cpp
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> ans;
+    void func(Node* root)
+    {
+        if(!root)
+            return;
+        if(root->children.size()==0)
+        {
+            ans.push_back(root->val);
+            return;
+        }
+        
+        for(auto it:root->children)
+            func(it);
+        ans.push_back(root->val);
+    }
+    vector<int> postorder(Node* root) 
+    {
+        func(root);
+        return ans;
+        
+    }
+};
