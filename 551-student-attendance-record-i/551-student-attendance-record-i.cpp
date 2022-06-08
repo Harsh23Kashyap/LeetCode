637536e391d74871a330766b0fd2c5c2d86b5e23
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2022-06-08 13:13:55 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (6.09%) - LeetHub

---
+++ 551-student-attendance-record-i/551-student-attendance-record-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkRecord(string s) 
+    {
+        int a=0,l=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='A')
+                a++;
+            if(s[i]=='L')
+                l++;
+            else
+                l=0;
+            if(a>1 or l>2)
+                return false;
+        }
+        return true;
+        
+    }
+};
