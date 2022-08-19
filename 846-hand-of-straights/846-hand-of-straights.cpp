90b9852180bf81fefd18273286cc280ed1e1d81a
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2022-08-20 03:08:28 +0530

    Time: 122 ms (53.49%), Space: 26.9 MB (87.13%) - LeetHub

---
+++ 846-hand-of-straights/846-hand-of-straights.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& nums, int k) 
+    {
+        sort(nums.begin(),nums.end());
+        if(nums.size()%k!=0)
+            return false;
+        unordered_map<int,int> am;
+        for(int i:nums)
+            am[i]++;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            //cout<<nums[i]<<endl;
+            if(am[nums[i]]<=0)
+                continue;
+            else 
+            {
+                for(int j=nums[i];j<nums[i]+k;j++)
+                {
+                    //cout<<j<<" ";
+                    if(am[j]==0)
+                        return false;
+                    am[j]--;
+                        
+                }
+            }
+            //cout<<endl;
+        }
+        return true;
+        
+    }
+};
