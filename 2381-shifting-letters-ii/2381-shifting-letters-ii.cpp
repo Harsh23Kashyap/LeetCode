aa6eb0bfde527ed97451b9f5380ced3b9cc3e2ab
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2022-08-21 16:52:06 +0530

    Time: 623 ms (60.00%), Space: 94 MB (60.00%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
@@ -1,23 +1,55 @@
 class Solution {
 public:
    
-string shiftingLetters(string s, vector<vector<int>>& shifts) {
-    int n=s.length();
-    vector<int> dir(n+1,0);
-    for(int k=0;k<shifts.size();k++){
-        int i = shifts[k][0], j=shifts[k][1], move = (shifts[k][2] == 0 ? -1:1);
-        dir[i] += move, dir[j+1] -= move;
+string shiftingLetters(string s, vector<vector<int>>& shifts) 
+    {
+        vector<int> sh(s.size()+1,0);
+        for(int i=0;i<shifts.size();i++)
+        {
+            int si=shifts[i][0],ei=shifts[i][1],dir=shifts[i][2];
+            dir=((dir==0)?-1:1);
+            sh[si]+=dir;
+            sh[ei+1]-=dir;
+        }
+        for(int i=1;i<sh.size();i++)
+            sh[i]+=sh[i-1];
+        string ans="";
+        for(int i=0;i<sh.size()-1;i++)
+        {
+            
+            char ch=s[i];
+            char cr='?';
+            int dir=sh[i]%26;
+            // cout<<i<<" - "<<dir<<endl;
+            // cout<<"Crrent-"<<ch<<endl;
+            //cout<<(int)(ch+dir)<<endl;
+            if((int)(ch+dir)>=97 and (int)(ch+dir)<=122)
+            {
+                //cout<<"runs1"<<endl;
+                cr=(char)(ch+dir);
+            }
+            else if((int)(ch+dir)<97)
+             {
+                 //cout<<"runs2"<<endl;
+                 dir=abs(dir);
+                
+                 dir=dir-(ch-'a')-1;
+                 //cout<<dir<<endl;
+                 cr=(char)('z'-dir);
+             }
+            else if((int)(ch+dir)>122)
+            {
+                // cout<<"runs3"<<endl;
+                 
+                //cout<<dir<<endl;
+                dir=dir-('z'-ch)-1;
+                //cout<<dir<<endl;
+                cr=(char)('a'+dir);
+            }
+           // cout<<cr<<endl;
+            ans+=cr;
+        }
+        return ans;
+        
     }
-    
-    for(int i=1;i<n+1;i++) dir[i] += dir[i-1]; // cummulative effect of all shifts on each index 
-    
-    for(int i=0;i<n;i++){
-        int idx = (s[i]-'a' + dir[i])%26;
-        if(idx<0) idx += 26;
-        char new_ch = (char)(idx + 'a');
-        s[i] = new_ch;
-    }
-    return s;
-}
-
 };
