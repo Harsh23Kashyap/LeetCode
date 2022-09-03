44f44eb0444b24f6f93cc040f92212519cc8757f
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2022-09-03 23:07:26 +0530

    Time: 266 ms (100.00%), Space: 44.1 MB (100.00%) - LeetHub

--- 2397-maximum-rows-covered-by-columns/2397-maximum-rows-covered-by-columns.cpp
+++ 2397-maximum-rows-covered-by-columns/2397-maximum-rows-covered-by-columns.cpp
@@ -17,69 +17,76 @@ public:
     int maximumRows(vector<vector<int>>& mat, int cols) 
     {
         
-                int n=mat.size();
-                int m=mat[0].size();
+        int n=mat.size();
+        int m=mat[0].size();
+        
+        int possible=power(2,m)-1;
+        vector<vector<int>> vp(possible+1,vector<int>(m,0));
 
-                int possible=power(2,m)-1;
-                vector<vector<int>> vp(possible+1,vector<int>(m,0));
-
-                int i=0;
-                while(i<=possible)
+        int i=0;
+        while(i<=possible)
+        {
+             int el=i;
+            int j=0;
+            while(j<m)
+            {
+                if((i&(1<<j))>=1)
                 {
-                     int el=i;
-                    int j=0;
-                    while(j<m)
-                    {
-                        if((i&(1<<j))>=1)
-                        {
-                            vp[i][j]=1;
-                        }
-                        j++;
-                    }
-                    i++;
-                }
-                int maxi=INT_MIN;
-                i=0;
-                while(i<=possible)
+                    vp[i][j]=1;
+                }
+                j++;
+            }
+            i++;
+        }
+        int maxi=INT_MIN;
+        i=0;
+        
+        
+        
+        
+        
+        
+        
+        while(i<=possible)
+        {
+            int cnt=0;
+            set<int> st;
+            int gnt=0;
+            int j=0;
+            while(j<m)
+            {
+                 if(vp[i][j]==1)
+                 {
+                     st.insert(j);
+                     cnt++;
+                 }
+                j++;
+             }
+            if(cnt==cols)
+            {
+
+                for(int i=0;i<n;i++)
                 {
-                    int cnt=0;
-                    set<int> st;
-                    int gnt=0;
-                    int j=0;
-                    while(j<m)
+                   int check=0; 
+                    for(int j=0;j<m;j++)
                     {
-                         if(vp[i][j]==1)
-                         {
-                             st.insert(j);
-                             cnt++;
-                         }
-                        j++;
-                     }
-                    if(cnt==cols)
-                    {
-
-                        for(int i=0;i<n;i++)
+                        if(st.count(j)==0)
                         {
-                           int check=0; 
-                            for(int j=0;j<m;j++)
-                            {
-                                if(st.count(j)==0)
-                                {
-                                    if(mat[i][j]==1) 
-                                        check=1;
-                                }
-                            }
-                           if(check==0)
-                               gnt++; 
-                        }
-
-                    } 
-                  maxi=max(maxi,gnt);
-                    i++;
-                }
-                if(maxi==INT_MIN)
-                    return 0;
-                return maxi;
+                            if(mat[i][j]==1) 
+                                check=1;
+                        }
+                    }
+                   if(check==0)
+                       gnt++; 
+                }
+                
+            } 
+          maxi=max(maxi,gnt);
+            i++;
+        }
+        if(maxi==INT_MIN)
+            return 0;
+        return maxi;
         
     }
 };
