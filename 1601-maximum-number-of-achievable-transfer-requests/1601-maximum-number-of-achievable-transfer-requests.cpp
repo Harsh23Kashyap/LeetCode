b2f723d3429ee5b1f9a049ed270d7ac3219c7a4b
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2023-07-03 00:04:06 +0530

    Time: 108 ms (67.65%), Space: 8.9 MB (51.96%) - LeetHub

--- 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
-    int answer = 0;
-
-    void maxRequest(vector<vector<int>>& requests, vector<int>& indegree, int n, int index, int count) {
-        if (index == requests.size()) {
-            // Check if all buildings have an in-degree of 0.
-            for (int i = 0; i < n; i++) {
-                if (indegree[i]) {
-                    return;
+    int maxi=0;
+        void pass(vector<vector<int>>& requests, int i, int total, vector<int>& dp)
+    {
+        if(i==requests.size())
+        {
+            
+            for(int i:dp)
+            {
+                if(i!=0)
+                {
+                   return;
                 }
             }
-            
-            answer = max(answer, count);
+            maxi=max(maxi,total);
             return;
         }
         
-        // Consider this request, increment and decrement for the buildings involved.
-        indegree[requests[index][0]]--;
-        indegree[requests[index][1]]++;
-        // Move on to the next request and also increment the count of requests.
-        maxRequest(requests, indegree, n, index + 1, count + 1);
-        // Backtrack to the previous values to move back to the original state before the second recursion.
-        indegree[requests[index][0]]++;
-        indegree[requests[index][1]]--;
-        
-        // Ignore this request and move on to the next request without incrementing the count.
-        maxRequest(requests, indegree, n, index + 1, count);
+        dp[requests[i][0]]--;
+        dp[requests[i][1]]++;
+        pass(requests,i+1,total+1,dp);
+        dp[requests[i][0]]++;
+        dp[requests[i][1]]--;
+            pass(requests,i+1,total,dp);
     }
-    
     int maximumRequests(int n, vector<vector<int>>& requests) {
-        vector<int> indegree(n, 0);
-        maxRequest(requests, indegree, n, 0, 0);
-        
-        return answer;
+        vector<int> dp(n,0);
+        pass(requests,0,0,dp);
+        return maxi;
     }
 };
