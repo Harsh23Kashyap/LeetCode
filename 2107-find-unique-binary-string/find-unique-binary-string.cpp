76b7dc4c4ac23b75da95357e086d688d2ddcc79a
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2025-02-20 10:12:19 +0530

    Time: 11 ms (16.35%) | Memory: 21.5 MB (13.53%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    string ans="";
-    void recur(int n, unordered_set<string> a, string s)
-    {
-        if(s.size()==n)
-        {
-            if(a.find(s)==a.end())
-            {
-                ans=s;
-                
-            }
-            return ;
-        }
-        if(ans!="")
-            return;
-        recur(n,a,s+"1");
-        if(ans!="")
-            return;
-        recur(n,a,s+"0");
-        
+string ans="";
+bool pass(string res, int i,int len,unordered_set<string> u){
+    if(u.find(res)==u.end()){
+        // cout<<res<<" works"<<endl;
+        ans=res;
+        return true;
     }
+    if(i==len)
+        return false;
+
+    //modify current
+    bool check=pass(res,i+1,len,u);
+    res[i]='1';
+    check=check&pass(res,i+1,len,u);
+    return check;
+
+}
     string findDifferentBinaryString(vector<string>& nums) {
-unordered_set<string> a(nums.begin(),nums.end());
         
-        recur(nums.size(),a,"");
-            return ans;
+        unordered_set<string> u(nums.begin(),nums.end());
+
+        string res(nums[0].size(),'0');
+        cout<<res<<endl;
+        pass(res,0,nums[0].size(),u);
+        return ans;
     }
 };
