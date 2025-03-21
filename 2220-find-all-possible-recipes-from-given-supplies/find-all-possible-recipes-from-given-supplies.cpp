95e3f05697e1e6e1978f6f87362c37a8c53831f1
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2025-03-21 15:37:51 +0530

    Time: 131 ms (54.60%) | Memory: 165.7 MB (22.75%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -3,11 +3,10 @@ public:
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
         vector<string> ans;
         unordered_set<string> rc(recipes.begin(), recipes.end());
-        unordered_map<string, int> in;  // Stores the in-degree (dependencies count)
+        unordered_map<string, int> in;  
         queue<string> q;   
         unordered_map<string, vector<string>> edges; 
 
-        // Build graph and in-degree map
         for(int i = 0; i < recipes.size(); i++) {
             for(auto &j : ingredients[i]) {
                 edges[j].push_back(recipes[i]); 
@@ -15,24 +14,22 @@ public:
             }
         }
 
-        // Initialize queue with available supplies
         for(auto &supply : supplies) {
             q.push(supply);
         }
         
-        // Process recipes using topological sorting (BFS)
         while(!q.empty()) {
             string item = q.front();
             q.pop();
 
-            if(rc.count(item)) { // If it's a recipe, add to answer
+            if(rc.count(item)) { 
                 ans.push_back(item);
             }
 
-            for(auto &recipe : edges[item]) { // Reduce in-degree of dependent recipes
+            for(auto &recipe : edges[item]) { 
                 in[recipe]--;
                 if(in[recipe] == 0) {
-                    q.push(recipe); // If all ingredients are available, add to queue
+                    q.push(recipe);
                 }
             }
         }
