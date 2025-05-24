5cfb70b349b4d7910c7505ce11ae97f5c4d0eb6e
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2025-05-24 12:45:12 +0530

    Time: 104 ms (34.41%) | Memory: 105.9 MB (21.08%) - LeetSync

--- 2417-the-latest-time-to-catch-a-bus/the-latest-time-to-catch-a-bus.cpp
+++ 2417-the-latest-time-to-catch-a-bus/the-latest-time-to-catch-a-bus.cpp
@@ -1,86 +1,42 @@
 class Solution {
 public:
-    int latestTimeCatchTheBus(vector<int>& buses, vector<int>& p, int capacity) {
-        sort(buses.begin(),buses.end());
-        sort(p.begin(),p.end());
-        int c=buses.size();
-        unordered_set<int> u;
-        int last=0;
-
-        int i=0,j=0;
-        int cc=0;
-//         vector<vector<int>> pp;
-// vector<int> t;
-        while(i<buses.size() and j<p.size()){
-            // cout<<i<<" "<<j<<endl;
-            if(p[j]<=buses[i]){
-                if(cc<capacity){
-                    
-                    // t.push_back(p[j]);
-                    last=p[j];
-                    cout<<last<<" ";
-                    u.insert(last);
-                    cc++;
-                    // cout<<"Pushing"<<" "<<cc<<endl;
-                    j++;
-                }
-                else{
-                    if(i+1==buses.size())
-                    break;
-                    last=p[j];
-                    cout<<"\n"<<last<<" ";
-                    u.insert(last);
-                    cc=1;
-                    i++;
-                    j++;
-
-                }
-            }
-            else {
-
-               cout<<endl;
-               if(cc<capacity){
-                last=abs(buses[i]);
-                u.insert(-1*last);
-               }
-                cc=0;
-
-               i++;
-
-            }
-        }
-        if(j==p.size()){
-            if(i<buses.size()-1){
-            last=buses[buses.size()-1];
-            }
-            else if(i==buses.size()-1){
-                if(cc!=capacity)
-                    last=buses[buses.size()-1];
-            }
-        }
-        // pp.push_back(t);
-        cout<<"Bus Passengers"<<" "<<last<<endl;
-        // for(auto it:pp){
-        //     for(auto j:it)
-        //     cout<<j<<" ";
-        //     cout<<endl;
-        // }
-for(auto it:u){
-    cout<<it<<" ";
-}
-cout<<endl;
-        for(int i=last;i>=0;i--){
-            if(u.find(i)!=u.end())
-                continue;
-            if(u.find(i)==u.end())
-                return i;
-             if(u.find(-1*i)!=u.end())
-                return i;
+    int latestTimeCatchTheBus(vector<int>& buses, vector<int>& passengers, int capacity) {
+        sort(buses.begin(), buses.end());
+        sort(passengers.begin(), passengers.end());
+        
+        unordered_set<int> taken(passengers.begin(), passengers.end());
+        
+        int j = 0; // passenger index
+        int lastBusTime = 0, lastBoarded = 0;
+        
+        for (int i = 0; i < buses.size(); i++) {
+            int currBus = buses[i];
+            int cap = 0;
             
+            while (j < passengers.size() && passengers[j] <= currBus && cap < capacity) {
+                lastBoarded = passengers[j];
+                j++;
+                cap++;
+            }
+
+            // If it's the last bus, handle answer logic after loop
+            if (i == buses.size() - 1) {
+                if (cap < capacity) {
+                    // Try latest time ≤ bus that isn't taken
+                    for (int t = currBus; t >= 0; t--) {
+                        if (!taken.count(t))
+                            return t;
+                    }
+                } else {
+                    // Bus full, try just before lastBoarded
+                    for (int t = lastBoarded - 1; t >= 0; t--) {
+                        if (!taken.count(t))
+                            return t;
+                    }
+                }
+            }
         }
-        return -1;
 
-
-
+        return -1; // Should never reach here per problem constraints
     }
 };
