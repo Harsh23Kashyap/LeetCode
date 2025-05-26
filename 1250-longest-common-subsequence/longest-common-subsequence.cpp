cfd6c2bf39575d8cd6c452c5d3630806fb84da89
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2025-05-26 21:44:27 +0530

    Time: 77 ms (5.03%) | Memory: 27.5 MB (27.77%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    int longestPossible(vector<vector<int>>& dp, const string &t1, const string &t2, int i, int j) {
-        if(i == (int)t1.size() || j == (int)t2.size())
-            return 0;
+    int longestPossible(vector<vector<int>>& dp, string &t1,string &t2,int i,int j){
+        if(i==t1.size() or j==t2.size())
+        return 0;
 
-        if(dp[i][j] != -1)
-            return dp[i][j];
+        if(dp[i][j]!=-1)
+        return dp[i][j];
 
-        int ans = 0;
-        if(t1[i] == t2[j])
-            ans = 1 + longestPossible(dp, t1, t2, i + 1, j + 1);
+        int ans=0;
+        if(t1[i]==t2[j])
+            ans=1+longestPossible(dp,t1,t2,i+1,j+1);
+        if(i+1<t1.size())
+        ans=max(ans,longestPossible(dp,t1,t2,i+1,j));
+        if(j+1<t2.size())
+        ans=max(ans,longestPossible(dp,t1,t2,i,j+1));
+        return dp[i][j]=ans;
 
-        ans = max(ans, longestPossible(dp, t1, t2, i + 1, j));
-        ans = max(ans, longestPossible(dp, t1, t2, i, j + 1));
-
-        return dp[i][j] = ans;
     }
-
     int longestCommonSubsequence(string text1, string text2) {
-        int n = (int)text1.size();
-        int m = (int)text2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        return longestPossible(dp, text1, text2, 0, 0);
+        vector<vector<int>> dp(text1.size(),vector<int> (text2.size(),-1));
+
+        return longestPossible(dp,text1,text2,0,0);
+
     }
 };
