3e0b59eaea5c970f3d7b8653dddb77324f25437f
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2025-06-09 23:39:06 +0530

    Time: 3 ms (80.09%) | Memory: 9.7 MB (83.00%) - LeetSync

--- 836-race-car/race-car.cpp
+++ 836-race-car/race-car.cpp
@@ -1,66 +1,38 @@
 class Solution {
 public:
-struct pair_hash {
-    size_t operator()(const std::pair<int, int>& p) const {
-        return ((size_t)p.first << 16) ^ (size_t)p.second;
-    }
-};
-
+  unordered_map<int, int> memo;
     int racecar(int target) {
-        if(target==0)
-            return 0;
-        queue<pair<long long,long long>> q;
-       set<pair<int, int>> u;
-        q.push({0,1});
-        u.insert({0,1});
-        int c=0;
-        while(!q.empty()){
-            int sz=q.size();
-
-            while(sz--){
-                auto it=q.front();
-                // cout<<it.first<<" "<<it.second<<endl;
-                q.pop();
-                //lets accelerate
-                long long nx=it.first+it.second;
-                long long ns=it.second*2;
-                if(nx==target)
-                    return c+1;
-                
-                // s=to_string(nx)+" . "+to_string(ns);
-                if(u.find({nx,ns})==u.end() and nx<=2*target){
-                    q.push({nx,ns});
-                    u.insert({nx,ns});
-                }
-
-                //revrse
-                if(it.second>0)
-                    ns=-1;
-                else
-                    ns=1;
-                // s=to_string(it.first)+" . "+to_string(ns);
-                if(u.find({it.first,ns})==u.end() and it.first<=2*target ){
-                    q.push({it.first,ns});
-                    u.insert({it.first,ns});
-                }
-
-            }
-            c++;
-            // cout<<endl;
+        if (memo.count(target)) 
+            return memo[target];
+        
+        int n = 1;
+        
+        while ((1 << n) - 1 < target) 
+            n++;
+        int overshoot=(1 << n) - 1;
+        if (overshoot == target) {
+            memo[target] = n;
+            return n;
         }
-        return -1;
+        int res=INT_MAX;
+        if(memo.find(overshoot - target)==memo.end())
+             res = min(res, n + 1 + racecar(overshoot-target));
+        else
+            res = min(res, n + 1 + memo[overshoot-target]);
+
+        int undershoot = (1 << (n - 1)) - 1;
+        
+        for (int m = 0; m < n - 1; ++m) {
+            int forwardDist = undershoot;
+
+            int backDist = (1 << m) - 1; 
+
+            int steps = (n - 1) + 1 + m + 1 + racecar(target - forwardDist + backDist);
+             res = min(res, steps);
+        }
+         memo[target] = res;
+        return res;
+        
 
     }
 };
-
-
-/*
-A
-1.   2
-A
-3.   4
-R
-3.   -1
-
-
-*/
