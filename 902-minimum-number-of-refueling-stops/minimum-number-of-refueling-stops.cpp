170d10251af6801cefdadb2ba84f8bf90273c3d1
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2025-06-20 22:51:40 -0700

    Time: 8 ms (23.45%) | Memory: 20.2 MB (52.48%) - LeetSync

--- 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.cpp
+++ 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.cpp
@@ -1,61 +1,26 @@
 class Solution {
 public:
-    // int ds(int curr, int target, int fuel, vector<vector<int>> u, int  sp){
-
-    //     if(curr+fuel>=target)
-    //     return 0;
-
-    //    for(int k=sp;k<u.size();k++){
-
-    //         if(sp+fuel<u[k].first)
-    //             break;
-            
-    //         //buy petrol here and move
-    //         int notbuy=
-    //         //dont buy petrol here
-    //    }
-
-    // }
     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
-        sort(stations.begin(),stations.end());
-        // for(auto it:stations)
-        //     u[it.first]=it.second;
-        // return ds(0,target,startFuel,stations,0)
-        vector<int> vis(stations.size(),false);
-        int j=0;
-        priority_queue<int> pq;
-        int start=0;
-        int c=0;
-        while(startFuel>0){
+        int n = stations.size();
+        vector<long long> dp(n + 1, 0);
+        dp[0] = startFuel;
 
-            int maxDist=start+startFuel;
-            cout<<maxDist<<endl;
-            if(maxDist>=target)
-            return c;
-            cout<<"CUrrently at "<<maxDist<<endl;
-            while(j<stations.size()){
-                if(stations[j][0]<=maxDist)
-                {
-                    pq.push(stations[j][1]);
-                    cout<<"Added "<<stations[j][0]<<endl;
-                    j++;
+        for (int i = 0; i < n; i++) {
+            int pos = stations[i][0], fuel = stations[i][1];
+
+            // Go backwards to prevent overwriting dp[j]
+            for (int j = i; j >= 0; j--) {
+                if (dp[j] >= pos) {
+                    dp[j + 1] = max(dp[j + 1], dp[j] + fuel);
                 }
-                else break;
             }
-
-            start=maxDist;
-
-            startFuel=0;
-            if(pq.size()>0){
-            startFuel+=pq.top();
-            cout<<"Fuelded up "<<startFuel<<endl;
-            pq.pop();
-            c++;
-            }
-
-
-
         }
+
+        for (int i = 0; i <= n; i++) {
+            if (dp[i] >= target)
+                return i;
+        }
+
         return -1;
     }
 };
