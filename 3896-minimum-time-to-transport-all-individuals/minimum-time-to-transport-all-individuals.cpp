377ad0e4afc4c6632b4515ea8c53916b4dc0c380
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2025-07-03 12:12:49 +0530

    Time: 2722 ms (5.08%) | Memory: 334.7 MB (8.39%) - LeetSync

--- 3896-minimum-time-to-transport-all-individuals/minimum-time-to-transport-all-individuals.cpp
+++ 3896-minimum-time-to-transport-all-individuals/minimum-time-to-transport-all-individuals.cpp
@@ -1,76 +1,99 @@
 class Solution {
 public:
-    struct State {
-        double time;
-        int stage;
+int counter(int n){
+    int c=0;
+    while(n>0){
+        n=n&(n-1);
+        c++;
+    }
+    return c;
+}
+    struct Event{
+        double time; 
         int mask;
+        int stage;
         bool atBase;
-    };
 
-    struct CompareState {
-        bool operator()(const State& a, const State& b) const {
-            return a.time > b.time; // min-heap based on time
+    };
+    struct Comparator{
+        bool operator()(const Event &a, const Event &b){
+            return  a.time>b.time;
         }
     };
-
     double minTime(int n, int k, int m, vector<int>& time, vector<double>& mul) {
-        if (k == 1) return (n == 1) ? time[0] * mul[0] : -1.0;
+        if(k==1){
+            if(n==1)
+                return mul[0]*time[0];
+            return -1;
 
-        int fullMask = (1 << n) - 1;
-        priority_queue<State, vector<State>, CompareState> pq;
-        vector<vector<vector<double>>> dp(1 << n, vector<vector<double>>(m, vector<double>(2, 1e18)));
+        }
 
-        pq.push({0.0, 0, fullMask, true});
-        dp[fullMask][0][1] = 0.0;
+        int fullMask=(1<<n)-1;
+        priority_queue<Event, vector<Event>, Comparator> pq;
 
-        while (!pq.empty()) {
-            auto [currTime, stage, mask, atBase] = pq.top();
+        vector<vector<vector<double>>> dp(1<<n, vector<vector<double>> (m, vector<double> (2,2e15)));
+
+        pq.push({0.0,fullMask,0,true});
+
+        dp[fullMask][0][1] = 0;
+
+        while(!pq.empty()){
+            auto [currtime,currmask,stage,atBase]=pq.top();
             pq.pop();
+            if(currmask==0 and !atBase)
+                return currtime;
 
-            if (mask == 0 && !atBase) return currTime;
+            if(dp[currmask][stage][atBase]<currtime)
+                continue;
 
-            if (dp[mask][stage][atBase] < currTime) continue;
-
-            if (atBase) {
-                // Generate all subsets (non-empty, up to size k)
-                for (int submask = mask; submask; submask = (submask - 1) & mask) {
-                    int count = __builtin_popcount(submask);
-                    if (count == 0 || count > k) continue;
-
-                    int maxT = 0;
-                    for (int i = 0; i < n; ++i) {
-                        if ((submask >> i) & 1) {
-                            maxT = max(maxT, time[i]);
+            if(atBase)
+            {
+            for (int submask = currmask; submask > 0; submask = (submask - 1) & currmask){
+                    int tc=counter(submask);
+                    if(tc==0 or tc>k)
+                        continue;
+                    
+                    int maxTime=0;
+                    for(int bt=0;bt<n;bt++){
+                        if(((submask>>bt)&1) == 1){
+                            maxTime=max(maxTime,time[bt]);
                         }
                     }
 
-                    double tripTime = maxT * mul[stage];
-                    int nextStage = (stage + (int)floor(tripTime)) % m;
-                    int newMask = mask ^ submask;
+                    double tripTime=mul[stage]*maxTime;
 
-                    if (dp[newMask][nextStage][0] > currTime + tripTime) {
-                        dp[newMask][nextStage][0] = currTime + tripTime;
-                        pq.push({currTime + tripTime, nextStage, newMask, false});
+                    int nextStage=  (stage + (int)floor(tripTime)) % m;
+
+                    int newmask= currmask^submask;
+
+                    if(dp[newmask][nextStage][0]>tripTime){
+                        dp[newmask][nextStage][0] = currtime + tripTime;
+                        pq.push({currtime + tripTime,newmask, nextStage, false});
                     }
                 }
 
-            } else {
-                // Return one person who already crossed
-                for (int i = 0; i < n; ++i) {
-                    if (((mask >> i) & 1) == 0) { // crossed
-                        int newMask = mask | (1 << i);
+            }
+
+            else{
+                for (int i = 0; i < n; i++) 
+                {
+                    if (((currmask >> i) & 1) == 0) 
+                    { 
+                        int newmask = currmask | (1 << i);
                         double returnTime = time[i] * mul[stage];
                         int nextStage = (stage + (int)floor(returnTime)) % m;
 
-                        if (dp[newMask][nextStage][1] > currTime + returnTime) {
-                            dp[newMask][nextStage][1] = currTime + returnTime;
-                            pq.push({currTime + returnTime, nextStage, newMask, true});
+                        if (dp[newmask][nextStage][1] > currtime + returnTime) {
+                            dp[newmask][nextStage][1] = currtime + returnTime;
+                            pq.push({currtime + returnTime, newmask, nextStage, true});
                         }
                     }
                 }
-            }
-        }
 
-        return -1.0;
+            }
+            
+        }
+        return -1;
+        
     }
 };
