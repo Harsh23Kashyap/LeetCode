438e3f4c9f7d5591fe6496268c1c79579efdccfc
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2025-12-12 11:41:52 +0530

    Time: 240 ms (6.38%) | Memory: 42.9 MB (61.70%) - LeetSync

---
+++ 3721-count-mentions-per-user/count-mentions-per-user.cpp
@@ -0,0 +1,80 @@
+class Solution {
+public:
+    vector<int> mentions;
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> offline;
+    vector<int> online;
+
+    void update_mention(string men){
+         if(men=="HERE"){
+                for(int i=0;i<online.size();i++){
+                    if(online[i]==1)
+                        mentions[i]++;
+                }
+            }
+            else if(men=="ALL"){
+                for(int i=0;i<online.size();i++){
+                        mentions[i]++;
+                }
+            }
+            else{
+                stringstream ss(men);
+                string id;
+                while(ss>>id){
+                        int nid=stoi(id.substr(2));
+                        cout<<nid<<endl;
+                            mentions[nid]++;
+                    
+                }
+            }
+    }
+    static bool cmp(vector<string> &a, vector<string> &b){
+        int ta=stoi(a[1]);
+        int tb=stoi(b[1]);
+        if (ta<tb){
+            return true;
+
+        }
+        if(ta>tb){
+            return false;
+        }
+        if(ta==tb){
+            if(a[0]=="OFFLINE" and b[0]=="MESSAGE")
+            return true;
+
+             if(a[0]=="MESSAGE" and b[0]=="OFFLINE")
+            return false;
+
+        }
+        return true;
+    }
+    vector<int> countMentions(int numberOfUsers, vector<vector<string>>& events) {
+        mentions.resize(numberOfUsers);
+        vector<int> temp(numberOfUsers,1);
+        online=temp;
+
+        sort(events.begin(),events.end(),cmp);
+
+        for(int i=0;i<events.size();i++){
+            // cout<<"Run "<<i<<endl;
+            int currtime=stoi(events[i][1]);
+            while(!offline.empty() and currtime>=offline.top().first){
+                auto it=offline.top();
+                offline.pop();
+                online[it.second]=1;
+            }
+
+            if(events[i][0]=="MESSAGE"){
+                update_mention(events[i][2]);
+            }
+            else{
+            int user=stoi(events[i][2]);
+                offline.push({currtime+60,user});
+                online[user]=0;
+            }
+        }
+
+        return mentions;
+
+
+    }
+};
