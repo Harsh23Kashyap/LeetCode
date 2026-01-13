d796f4d17633e5a9eacdb88b60ac9a410d497d20
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-01-13 13:37:09 +0000

    Time: 0 ms (100.00%) | Memory: 10.5 MB (78.33%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
-    int c(int a){
-        int c=0;
-        while(a>0){
-            a=a&(a-1);
-            c++;
-        }
-        return c;
-    }
     vector<int> countBits(int n) {
-       vector<int> ans;
-       for(int i=0;i<=n;i++){
-        ans.push_back(c(i));
-       }
-       return ans;
+        vector<int> ans(n+1,0);
+
+        for(int i=1;i<=n;i++){
+            ans[i]=ans[i&(i-1)]+1;
+        }
+        return ans;
     }
 };
