bdd1a6ed88f0f318904006c3b7298e5c859e825d
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-01-17 08:49:42 +0000

    Time: 108 ms (46.69%) | Memory: 59.4 MB (56.20%) - LeetSync

--- 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.cpp
+++ 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
-long long largest(
-    vector<int>& al, vector<int>& au,
-    vector<int>& bl, vector<int>& bu
-) {
-    long long left   = max(al[0], bl[0]);
-    long long right  = min(au[0], bu[0]);
-    long long bottom = max(al[1], bl[1]);
-    long long top    = min(au[1], bu[1]);
-
-    if (left >= right || bottom >= top)
-        return 0;
-
-    long long side = min(right - left, top - bottom);
-    return side * side;
-}
 
     long long largestSquareArea(vector<vector<int>>& bottomLeft, vector<vector<int>>& topRight) {
         long long maxi=0;
         int n=bottomLeft.size();
         for(int i=0;i<n;i++)
         for(int j=i+1;j<n;j++){
-
-            long long c1=largest(bottomLeft[i],topRight[i],bottomLeft[j],topRight[j]);
-            // cout<<c1<<endl;
-            maxi=max(maxi,c1);
+                long long left   = max(bottomLeft[i][0], bottomLeft[j][0]);
+                long long right  = min(topRight[i][0], topRight[j][0]);
+                long long bottom = max(bottomLeft[i][1], bottomLeft[j][1]);
+                long long top    = min(topRight[i][1], topRight[j][1]);
+                if(left>right or top<bottom)
+                continue;
+                long long sz=min(right-left,top-bottom);
+            maxi=max(maxi,sz*sz);
         }
         return maxi;
     }
