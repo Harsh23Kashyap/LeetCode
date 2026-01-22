6a4d6adc124b042e42115f8236ac6b4d8560d2bf
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-01-22 07:26:26 +0000

    Time: 101 ms (6.34%) | Memory: 29.5 MB (8.13%) - LeetSync

---
+++ 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+bool nondecreasing(vector<int>& nums){
+cout<<nums[0]<<" ";
+    for(int i=1;i<nums.size();i++){
+        // cout<<nums[i]<<" ";
+        if(nums[i-1]>nums[i]){
+             cout<<endl;
+        return false;
+        }
+    }
+    cout<<endl;
+
+    return true;
+}
+    int minimumPairRemoval(vector<int>& nums) {
+        int a=0;
+        
+        while(!nondecreasing(nums)){
+            cout<<"SZ - "<<nums.size()<<endl;
+            a++;
+            long long mini=INT_MAX;
+            for(int i=0;i<nums.size()-1;i++){
+                if(nums[i]+nums[i+1]<mini)
+                mini=nums[i]+nums[i+1];
+            }
+            cout<<"MINI "<<mini<<endl;
+            vector<int> copy;
+            bool found=false;
+            int i = 0;
+            while (i < nums.size()) {
+                if (!found and i + 1 < nums.size() and nums[i] + nums[i+1] == mini) {
+                    copy.push_back(nums[i] + nums[i+1]);
+                    found = true;
+                    i += 2; 
+                } else {
+                    copy.push_back(nums[i]);
+                    i++;
+                }
+            }
+
+            cout<<endl;
+            nums=copy;
+
+
+        }
+        return a;
+
+    }
+};
+
+
+/*
+2,2,-1,3,-2,2,1,1,1,-1
+2,2,-1,3,0,1,1,1,-1
+
+
+*/
