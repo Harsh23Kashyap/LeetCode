523bd95322e97c53bbe1a3806943b4290c296a4b
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-01-31 07:22:01 +0000

    Time: 0 ms (100.00%) | Memory: 19.7 MB (88.80%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        char ch=letters[0];
-        for(auto it:letters){
-            if(it>target){
-                return it;
-            }
-        }
-        return ch;
+        // char ch=letters[0];
+        // for(auto it:letters){
+        //     if(it>target){
+        //         return it;
+        //     }
+        // }
+        // return ch;
+        target++;
+        cout<<target<<endl;
+        int it=lower_bound(letters.begin(),letters.end(),target)-letters.begin();
+        if(it==letters.size())
+            return letters[0];
+
+        return letters[it];
     }
 };
