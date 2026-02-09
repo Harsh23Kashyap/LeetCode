5d9fc09038c54778b5c89b9fe9980a27d494bfdd
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-02-09 20:10:35 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (28.51%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    int trap(vector<int>& height) 
-    {
-        int n=height.size();
-        vector<int> left(n,0);
-        vector<int> right(n,0);
-        left[0]=height[0];
-        right[n-1]=height[n-1];
-        for(int i=1;i<n;i++)
+    int trap(vector<int>& height) {
+        vector<int> left(height.size(),0);
+        vector<int> right(height.size(),0);
+        left[0]=height[0],right[right.size()-1]=height.back();
+        for(int i=1;i<height.size();i++)
             left[i]=max(left[i-1],height[i]);
-        for(int i=n-2;i>=0;i--)
+        for(int i=height.size()-2;i>=0;i--)
             right[i]=max(right[i+1],height[i]);
-        
+
         int tot=0;
-        for(int i=1;i<n-1;i++)
-        {
-            //cout<<i<<" - "<<left[i]<<" "<<right[i]<<endl;
-            int curr=min(left[i],right[i]);
-            tot+=max(0,curr-height[i]);
+
+        for(int i=0;i<height.size();i++){
+            int mindhoondho=min(left[i],right[i]);
+            tot+=mindhoondho-height[i];
         }
         return tot;
-        
+
     }
 };
