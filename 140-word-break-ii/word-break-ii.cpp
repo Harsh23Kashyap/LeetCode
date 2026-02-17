14c3a828899937100dcd6e6d900d561db36d6a5c
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-02-17 23:47:39 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (12.03%) - LeetSync

--- 140-word-break-ii/word-break-ii.cpp
+++ 140-word-break-ii/word-break-ii.cpp
@@ -1,62 +1,66 @@
 class Solution {
-    vector<string> ans;//Store all valid sentences
-    class trienode{
-        public:
-        char c;
-        int we;
-        trienode *child[26];
-        trienode(char c){
-            we = 0;
-            this->c = c;
-            for(int i=0;i<26;++i)
-                child[i]=NULL;
-        }
-    };
-    trienode *root;//root of TRIE
-    void insertInTrie(const string &word){
-        trienode *curr = root;
-        int idx;
-        for(int i=0;i<word.size();++i){
-            idx = word[i]-'a';
-            if(!curr->child[idx])
-                curr->child[idx] = new trienode(char(97+idx));
-            curr = curr->child[idx];
-        }
-        curr->we += 1;
-    }
-    bool searchInTrie(string s){
-        trienode *curr = root;
-        int idx;
-        for(int i=0;i<s.size();++i){
-            idx = s[i]-'a';
-            if(!curr->child[idx])
-                return false;
-            curr = curr->child[idx];
-        }
-        return curr->we>0;
-    }
-    
-    void solve(const string &s,string st,int pos){
-        if(pos==s.size()){
-            ans.push_back(st);
-            return;
-        }
-        st += " ";
-        for(int i=pos;i<s.size();++i){
-            if(searchInTrie(s.substr(pos,i+1-pos)))
-                solve(s,st+s.substr(pos,i+1-pos),i+1);
-        }
-    }
 public:
-    vector<string> wordBreak(string s, vector<string>& wordDict) {
-        root = new trienode('/');
-        for(auto word: wordDict)
-            insertInTrie(word);
-        
-        for(int i=0;i<s.size();++i){
-            if(searchInTrie(s.substr(0,i+1)))
-                solve(s,s.substr(0,i+1),i+1);
+
+    struct TrieNode {
+        unordered_map<char, TrieNode*> children;
+        bool isEnd = false;
+    };
+
+    TrieNode* root = new TrieNode();
+    unordered_map<int, vector<string>> memo;
+
+    void insert(string& word) {
+        TrieNode* node = root;
+        for (char c : word) {
+            if (!node->children.count(c))
+                node->children[c] = new TrieNode();
+            node = node->children[c];
         }
-        return ans;
+        node->isEnd = true;
+    }
+
+    vector<string> dfs(string& s, int start) {
+
+        if (memo.find(start)!=memo.end())
+            return memo[start];
+
+        if (start == s.size())
+            return {""};
+
+        vector<string> result;
+        TrieNode* node = root;
+
+        for (int i = start; i < s.size(); i++) {
+
+            if (!node->children.count(s[i]))
+                break;
+
+            node = node->children[s[i]];
+
+            if (node->isEnd) {
+
+                string word = s.substr(start, i - start + 1);
+
+                vector<string> suffixes = dfs(s, i + 1);
+
+                for (string &suffix : suffixes) {
+
+                    if (suffix == "")
+                        result.push_back(word);
+                    else
+                        result.push_back(word + " " + suffix);
+                }
+            }
+        }
+
+        return memo[start] = result;
+    }
+
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+
+        for (string word : wordDict)
+            insert(word);
+
+        return dfs(s, 0);
     }
 };
