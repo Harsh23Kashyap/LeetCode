cd1530e0d0318c0261f659378505eb8018d29d04
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-02-18 00:57:52 +0530

    Time: 129 ms (19.08%) | Memory: 80.6 MB (26.97%) - LeetSync

--- 2325-number-of-ways-to-select-buildings/number-of-ways-to-select-buildings.cpp
+++ 2325-number-of-ways-to-select-buildings/number-of-ways-to-select-buildings.cpp
@@ -1,38 +1,56 @@
 class Solution {
 public:
-    long long numberOfWays(string s) {
-        long long ans=0;
-        long long sufone=0;
-        long long sufzero=0;
-        long long prefone=0;
-        long long prefzero=0;
-        if(s[0]=='0')
-            prefzero++;
-        else
-            prefone++;
-        for(int i=1;i<s.size();i++){
-            if(s[i]=='0'){
-                sufzero++;
-            }
-            else{
-                sufone++;
-            }
+    vector<pair<char,long long>> diff;
+    long long dp[100005][3];
+    int m;
+
+    long long pass(int idx, int ver, string &pattern) {
+
+        if (ver == 3) return 1;
+        if (idx >= m) return 0;
+
+        if (dp[idx][ver] != -1)
+            return dp[idx][ver];
+
+        long long skip = pass(idx + 1, ver, pattern);
+        long long take = 0;
+
+        if (diff[idx].first == pattern[ver]) {
+            take = diff[idx].second *
+                   pass(idx + 1, ver + 1, pattern);
         }
 
-        for(int i=1;i<s.size()-1;i++){
-            if(s[i]=='0')
-            {
-                ans+=(1ll*prefone*sufone);
-                sufzero--;
-                prefzero++;
+        return dp[idx][ver] = skip + take;
+    }
+
+    long long solvePattern(string pattern) {
+        memset(dp, -1, sizeof(dp));
+        return pass(0, 0, pattern);
+    }
+
+    long long numberOfWays(string s) {
+
+        // build block compression
+        int n = s.size();
+        int i = 0;
+
+        while (i < n) {
+            char c = s[i];
+            long long count = 0;
+            while (i < n && s[i] == c) {
+                count++;
+                i++;
             }
-            else if(s[i]=='1')
-            {
-                ans+=(1ll*prefzero*sufzero);
-                sufone--;
-                prefone++;
-            }
+            diff.push_back({c, count});
         }
+
+        m = diff.size();
+
+        long long ans = 0;
+
+        ans += solvePattern("010");
+        ans += solvePattern("101");
+
         return ans;
     }
 };
