1706da4dad2e742d52fbae8f4250c3bc779d06ea
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-03-04 09:19:51 +0530

    Time: 5 ms (95.71%) | Memory: 149.7 MB (38.41%) - LeetSync

--- 2165-plates-between-candles/plates-between-candles.cpp
+++ 2165-plates-between-candles/plates-between-candles.cpp
@@ -1,42 +1,51 @@
 class Solution {
 public:
     vector<int> platesBetweenCandles(string s, vector<vector<int>>& queries) {
-        int n = s.size();
-        vector<int> plates(n, 0);
-        vector<int> leftCandle(n, -1);
-        vector<int> rightCandle(n, -1);
- 
-        plates[0] = (s[0] == '*');
-        for (int i = 1; i < n; i++)
-            plates[i] = plates[i-1] + (s[i] == '*');
- 
+         int n = s.size();
+        
+        vector<int> prefix(n,0);
+        vector<int> leftCandle(n,-1);
+        vector<int> rightCandle(n,-1);
+        
+        // prefix sum of plates
+        prefix[0] = (s[0] == '*');
+        for(int i=1;i<n;i++){
+            prefix[i] = prefix[i-1] + (s[i] == '*');
+        }
+        
+        // nearest candle on left
         int last = -1;
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '|') last = i;
+        for(int i=0;i<n;i++){
+            if(s[i] == '|') last = i;
             leftCandle[i] = last;
         }
-
+        
+        // nearest candle on right
         last = -1;
-        for (int i = n-1; i >= 0; i--) {
-            if (s[i] == '|') last = i;
+        for(int i=n-1;i>=0;i--){
+            if(s[i] == '|') last = i;
             rightCandle[i] = last;
         }
-
+        
         vector<int> ans;
-
-        for (auto q : queries) {
-            int l = q[0], r = q[1];
-
-            int start = rightCandle[l];
-            int end = leftCandle[r];
-
-            if (start == -1 || end == -1 || start >= end) {
+        
+        for(auto &q : queries){
+            
+            int L = q[0];
+            int R = q[1];
+            
+            int leftBoundary = rightCandle[L];
+            int rightBoundary = leftCandle[R];
+            
+            if(leftBoundary == -1 || rightBoundary == -1 || leftBoundary >= rightBoundary){
                 ans.push_back(0);
-            } else {
-                ans.push_back(plates[end] - plates[start]);
+            }
+            else{
+                int plates = prefix[rightBoundary] - prefix[leftBoundary];
+                ans.push_back(plates);
             }
         }
-
+        
         return ans;
     }
 };
