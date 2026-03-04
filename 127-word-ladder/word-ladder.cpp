2e5e7bd5f1e83a2c55e9d211800431838d007432
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-03-04 09:26:05 +0530

    Time: 47 ms (85.60%) | Memory: 19.7 MB (85.30%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,39 +1,51 @@
 class Solution {
 public:
-    void pass(queue<string>& q, string w, unordered_set<string> &vis, unordered_set<string>& words){
-
-        for(int i=0;i<w.size();i++){
-            char temp=w[i];
-            for(char ch='a';ch<='z';ch++){
-                w[i]=ch;
-                if(words.find(w)!=words.end()){
-                    if(vis.find(w)==vis.end()){
-                        vis.insert(w);
-                        q.push(w);
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        
+        unordered_set<string> wordSet(wordList.begin(), wordList.end());
+        
+        if(wordSet.find(endWord) == wordSet.end())
+            return 0;
+        
+        queue<string> q;
+        q.push(beginWord);
+        
+        int level = 1;
+        
+        while(!q.empty()){
+            
+            int size = q.size();
+            
+            while(size--){
+                
+                string word = q.front();
+                q.pop();
+                
+                if(word == endWord)
+                    return level;
+                
+                for(int i=0;i<word.size();i++){
+                    
+                    char original = word[i];
+                    
+                    for(char c='a'; c<='z'; c++){
+                        
+                        word[i] = c;
+                        
+                        if(wordSet.find(word) != wordSet.end()){
+                            
+                            q.push(word);
+                            wordSet.erase(word); // mark visited
+                        }
                     }
+                    
+                    word[i] = original;
                 }
             }
-            w[i]=temp;
+            
+            level++;
         }
-    }
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        queue<string> q;
-        unordered_set<string> words(wordList.begin(),wordList.end());
-        unordered_set<string> vis;
-        int c=1;
-        q.push(beginWord);
-        while(!q.empty()){
-            int sz=q.size();
-            while(sz--){
-                auto it=q.front();
-                q.pop();
-                if(it==endWord)
-                return c;
-                pass(q,it,vis,words);
-            }
-
-        c++;
-        }
+        
         return 0;
     }
 };
