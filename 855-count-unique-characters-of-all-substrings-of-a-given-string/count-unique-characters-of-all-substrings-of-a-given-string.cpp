98069da27d11bb2ac8eb2d969a17543ba60f3a34
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-03-05 23:21:42 +0530

    Time: 405 ms (5.36%) | Memory: 92.9 MB (5.36%) - LeetSync

--- 855-count-unique-characters-of-all-substrings-of-a-given-string/count-unique-characters-of-all-substrings-of-a-given-string.cpp
+++ 855-count-unique-characters-of-all-substrings-of-a-given-string/count-unique-characters-of-all-substrings-of-a-given-string.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
     int uniqueLetterString(string s) {
-        int n = s.size();
-        unordered_map<char, set<int>> mp;
+        
+        unordered_map<char,set<int>> u;
+        for(int i=0;i<s.size();i++)
+            u[s[i]].insert(i);
 
-        // store indices
-        for (int i = 0; i < n; i++) 
-            mp[s[i]].insert(i);
+
+        int count=0;
+
+        for(int i=0;i<s.size();i++){
+
+            set<int> &temp=u[s[i]];
+
+            //this is bsaically my current
+            //now all thse affected will be 
+            auto p=temp.find(i);
+            int previous=-1, nex=s.size();
+            if(p!=temp.begin())
+                previous=*prev(p);
+            if(next(p)!=temp.end())
+                nex=*next(p);
+
+            count+=((i-previous)*(nex-i));
+
+
+        }
+        return count;
         
 
-        long long ans = 0;
-
-        for (int i = 0; i < n; i++) {
-            auto &st = mp[s[i]];
-
-            auto it = st.find(i);
-
-            int prev = -1;
-            int next = n;
-
-            // previous occurrence
-            if (it != st.begin()) {
-auto pit = std::prev(it);
-                prev = *pit;
-            }
-
-            // next occurrence
-            auto nit = std::next(it);
-            if (nit != st.end()) {
-                next = *nit;
-            }
-
-            ans += 1LL * (i - prev) * (next - i);
-        }
-
-        return ans;
     }
 };
