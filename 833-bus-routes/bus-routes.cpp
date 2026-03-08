7362c050acd62ababae48d4a73d335243aac407d
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-03-08 16:08:52 +0530

    Time: 130 ms (74.30%) | Memory: 74.5 MB (28.93%) - LeetSync

--- 833-bus-routes/bus-routes.cpp
+++ 833-bus-routes/bus-routes.cpp
@@ -1,50 +1,41 @@
 class Solution {
 public:
     int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
-if (source == target) return 0;
-
-        unordered_map<int, vector<int>> uu;
-        for (int i = 0; i < routes.size(); ++i) {
-            for (int stop : routes[i]) {
-                uu[stop].push_back(i);
+        unordered_map<int,vector<int>> u;
+        for(int i=0;i<routes.size();i++){
+            for(int j=0;j<routes[i].size();j++){
+                u[routes[i][j]].push_back(i);
             }
         }
 
-        unordered_set<int> vss;
-        unordered_set<int> visitedBuses;
-        queue<int> q;
-        q.push(source);
-        vss.insert(source);
-        int curr=1;
+        unordered_set<int> bus;
+        unordered_map<int,int> currstops;
+
+        queue<pair<int,int>> q;
+        q.push({source,0});
         while(!q.empty()){
             int sz=q.size();
             while(sz--){
-
-                int currstop=q.front();
+                auto it=q.front();
                 q.pop();
-
-                for(auto nb:uu[currstop]){
-                    if(visitedBuses.find(nb)!=visitedBuses.end())
+                if(it.first==target)
+                return it.second;
+                if(currstops[it.first]<it.second)
                     continue;
 
-                    visitedBuses.insert(nb);
-
-                    vector<int> stopforbus=routes[nb];
-                    for(auto k:stopforbus){
-                        if(k==target)
-                        return curr;
-
-                        if(vss.find(k)!=vss.end()) continue;
-                        vss.insert(k);
-                        q.push(k);
-
+                for(auto allbus:u[it.first]){
+                    if(bus.count(allbus))
+                        continue;
+bus.insert(allbus);  
+                    for(int stops:routes[allbus]){
+                        if(currstops.find(stops)==currstops.end() or it.second+1<currstops[stops]){
+                            currstops[stops]=it.second+1;
+                            q.push({stops,it.second+1});
+                        }
                     }
                 }
             }
-            curr++;
-
         }
-
         return -1;
     }
 };
