c520b3cba8a39d594e2e8aa03b43ab1ded9abe5c
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-03-08 17:58:51 +0530

    Time: 222 ms (8.41%) | Memory: 14 MB (8.02%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
-int maxi=0;
-
-int pass(vector<vector<int>> &dp, int l, int r,vector<int>& nums){
+int pss(int l, int r, vector<int>& nums, vector<vector<int>> &dp){
+    if(r>=nums.size())
+        return 1;
     if(l<0)
     return 1;
-    if(r>nums.size())
-    return 1;
-    if(l>r)
-        return 0;
 
-    //we need to find valid k;
     if(dp[l][r]!=-1)
     return dp[l][r];
     int ans=0;
     for(int k=l+1;k<r;k++){
-            //if we chooose some k then l+1 and k-1 is left side
-            ans=max(ans, nums[l]*nums[r]*nums[k]+pass(dp,l,k,nums)+pass(dp,k,r,nums));
-        
+        int curr=pss(l,k,nums,dp)+pss(k,r,nums,dp)+nums[l]*nums[r]*nums[k];
+        ans=max(ans,curr);
     }
     return dp[l][r]=ans;
 }
+
     int maxCoins(vector<int>& nums) {
         nums.insert(nums.begin(),1);
         nums.push_back(1);
-        vector<vector<int>> dp(nums.size()+1, vector<int> (nums.size()+1,-1));
-
-        return pass(dp,0,nums.size()-1,nums);
+        vector<vector<int>> dp(nums.size(), vector<int> (nums.size(),-1));
+        return pss(0,nums.size()-1, nums,dp);
     }
 };
