ebc0e9c393842833139c80673fab4744ababe00f
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-03-10 18:06:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (11.89%) - LeetSync

--- 2710-minimum-operations-to-reduce-an-integer-to-0/minimum-operations-to-reduce-an-integer-to-0.cpp
+++ 2710-minimum-operations-to-reduce-an-integer-to-0/minimum-operations-to-reduce-an-integer-to-0.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
-    int pass(int n, vector<int>& pow){
+unordered_map<int,int> u;
+    int minOperations(int n) {
         if(n==0)
         return 0;
-        cout<<n<<endl;
-        auto lit=lower_bound(pow.begin(),pow.end(),n)-pow.begin();
-        auto hit=lower_bound(pow.begin(),pow.end(),n)-pow.begin();
-        if(pow[lit]==n)
-            return 1;
-        
-        return 1+min(pass(n-pow[lit-1],pow),pass(pow[hit]-n,pow));
-
-    }
-    int minOperations(int n) {
-        vector<int> pow(30,1);
-
-        for(int i=1;i<30;i++){
-            pow[i]=pow[i-1]<<1;
-        }
+        if(u.count(n))
+        return u[n];
+        int msb = 31 - __builtin_clz(n);
+        if((n&(n-1))==0)
+            return u[n] = 1;
+        int nh=1<<(msb+1);
+        int nl=1<<(msb);
 
 
-        return pass(n,pow);
-
-        
+        return u[n]=min(minOperations(n-nl),minOperations(nh-n))+1;
     }
 };
