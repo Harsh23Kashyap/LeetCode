1d0754882657a8b75d025f81031c2cafac283537
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-03-26 07:56:13 -0700

    Time: 184 ms (31.14%) | Memory: 22.8 MB (22.94%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,51 +1,46 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+unordered_set<string> words;
+
+    unordered_set<string> u;
+    void pass(string s, queue<string> &q){
         
-        unordered_set<string> wordSet(wordList.begin(), wordList.end());
-        
-        if(wordSet.find(endWord) == wordSet.end())
-            return 0;
-        
-        queue<string> q;
-        q.push(beginWord);
-        
-        int level = 1;
-        
-        while(!q.empty()){
-            
-            int size = q.size();
-            
-            while(size--){
-                
-                string word = q.front();
-                q.pop();
-                
-                if(word == endWord)
-                    return level;
-                
-                for(int i=0;i<word.size();i++){
-                    
-                    char original = word[i];
-                    
-                    for(char c='a'; c<='z'; c++){
-                        
-                        word[i] = c;
-                        
-                        if(wordSet.find(word) != wordSet.end()){
-                            
-                            q.push(word);
-                            wordSet.erase(word); // mark visited
-                        }
-                    }
-                    
-                    word[i] = original;
-                }
+        for(int i=0;i<s.size();i++){
+            char prev=s[i];
+            for(char ch='a';ch<='z';ch++){
+                s[i]=ch;
+                if(words.find(s)!=words.end() and u.find(s)==u.end())
+                    q.push(s);
             }
-            
-            level++;
+            s[i]=prev;
+
         }
-        
+    }
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        // (wordList.begin(),wordList.end());
+        for(auto it:wordList)
+            words.insert(it);
+        queue<string> q;
+        int c=1;
+        // bool found-=true;
+        q.push(beginWord);
+        while(!q.empty()){
+            int sz=q.size();
+            while(sz--){
+                auto it=q.front();
+                cout<<it<<endl;
+                if(it==endWord)
+                return c;
+                q.pop();
+                if(u.find(it)!=u.end())
+                    continue;
+                u.insert(it);
+                pass(it,q);
+
+            }
+            c++;
+        }
         return 0;
+
     }
 };
