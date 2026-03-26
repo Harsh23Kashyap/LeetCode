7acd11d807442417eaaf5d3b3c668239b76e197f
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-03-26 08:15:44 -0700

    Time: 413 ms (5.14%) | Memory: 92.9 MB (5.66%) - LeetSync

--- 855-count-unique-characters-of-all-substrings-of-a-given-string/count-unique-characters-of-all-substrings-of-a-given-string.cpp
+++ 855-count-unique-characters-of-all-substrings-of-a-given-string/count-unique-characters-of-all-substrings-of-a-given-string.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     int uniqueLetterString(string s) {
-        
-        unordered_map<char,set<int>> u;
+        unordered_map<int, set<int>> u;
         for(int i=0;i<s.size();i++)
             u[s[i]].insert(i);
-
-
-        int count=0;
-
+        int c=0;
         for(int i=0;i<s.size();i++){
+            auto &it=u[s[i]];
+            int n=s.size(),p=-1;
+            auto k=it.find(i);
+            if(k!=it.begin())
+                p=*prev(k);
+            if(next(k)!=it.end())
+                n=*next(k);
 
-            set<int> &temp=u[s[i]];
-
-            //this is bsaically my current
-            //now all thse affected will be 
-            auto p=temp.find(i);
-            int previous=-1, nex=s.size();
-            if(p!=temp.begin())
-                previous=*prev(p);
-            if(next(p)!=temp.end())
-                nex=*next(p);
-
-            count+=((i-previous)*(nex-i));
-
+            c+=(i-p)*(n-i);
 
         }
-        return count;
-        
+        return c;
 
+        
+        
     }
 };
