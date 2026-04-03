f99f82f98144d96c080612a06da04be8c2d53849
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-04-03 19:55:01 +0530

    Time: 111 ms (2.62%) | Memory: 12.4 MB (5.13%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -1,45 +1,62 @@
 class Solution {
 public:
     string reorganizeString(string s) {
+        priority_queue<pair<int,char>> pq;
         unordered_map<char,int> u;
-        for(auto it:s){
+
+        cout << "Building frequency map:\n";
+        for(auto it:s) {
             u[it]++;
         }
 
-        priority_queue<pair<int,char>> pq    ;
-        for(auto it:u){
-            pq.push({it.second, it.first});
+        for(auto it:u) {
+            cout << it.first << " -> " << it.second << "\n";
+            pq.push({it.second,it.first});
         }
-        string ans="";
-        
-        while(pq.size()>1){
-            auto [c1,s1]=pq.top();
+
+        string res="";
+        cout << "\nStarting PQ processing...\n";
+
+        while(pq.size()>=2){
+            cout << "\nPQ size: " << pq.size() << "\n";
+
+            
+
+            auto it=pq.top();
             pq.pop();
-            auto [c2,s2]=pq.top();
+            cout << "Top1: " << it.second << " freq=" << it.first << "\n";
+
+            auto sit=pq.top();
             pq.pop();
+            cout << "Top2: " << sit.second << " freq=" << sit.first << "\n";
 
+            cout << "Appending: " << it.second << " and " << sit.second << "\n";
+            res+=it.second;
+            res=res+sit.second;
 
-            ans+=s1;
-            ans+=s2;
-            c1--,c2--;
-
-            if(c1>0){
-                pq.push({c1,s1});
-            }
-            if(c2>0){
-                pq.push({c2,s2});
+            if(it.first-1>0) {
+                cout << "Pushing back: " << it.second << " freq=" << it.first-1 << "\n";
+                pq.push({it.first-1,it.second});
             }
 
+            if(sit.first-1>0) {
+                cout << "Pushing back: " << sit.second << " freq=" << sit.first-1 << "\n";
+                pq.push({sit.first-1,sit.second});
+            }
 
+            cout << "Current result: " << res << "\n";
         }
 
-        if(!pq.empty()){
-            auto [c1,s1]=pq.top();
-            if(c1>1){
-                return "";
+        if(pq.size()==1) {
+                cout << "Only one element left: "
+                     << pq.top().second << " freq=" << pq.top().first << "\n";
             }
-            ans+=s1;
-        }
-        return ans;
-    }   
+
+        if(pq.size()==1 and pq.top().first>1)
+            return "";
+        else if(pq.size()==1 and pq.top().first==1)
+            res+=pq.top().second;
+        cout << "\nFinal result: " << res << "\n";
+        return res;
+    }
 };
