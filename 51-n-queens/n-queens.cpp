91363550d2d7dc1ad57a6bb2015ddc88369fdfad
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-04-03 20:19:50 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (37.36%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
 vector<bool> col;
+unordered_map<int,bool> ldiag;
+unordered_map<int,bool> rdiag;
 bool possible( vector<string> &a, int i, int k){
 
-    int tr=i-1;
-    int tc=k-1;
-    while(tc>=0 and tr>=0){
-        if(a[tr][tc]=='Q')
-            return false;
-            tr--,tc--;
-    }
-    tr=i-1,tc=k+1;
-    while(tr>=0 and tc<a.size()){
-        if(a[tr][tc]=='Q')
-            return false;
-            tr--,tc++;
-    }
+   if(not(ldiag.find(i-k)==ldiag.end() or (ldiag.find(i-k)!=ldiag.end() and ldiag[i-k]==false)))
+    return false;
+
+ if(not(rdiag.find(i+k)==rdiag.end() or (rdiag.find(i+k)!=rdiag.end() and rdiag[i+k]==false)))
+    return false;
+
     return true;
+    
+
 }
     void pass(vector<vector<string>>&ans,  vector<string> &a, int i){
         if(i==a.size()){
             ans.push_back(a);
+            return;
         }
         
         for(int k=0;k<a.size();k++){
             if(col[k]==false and possible(a,i,k)){
                 a[i][k]='Q';
                 col[k]=true;
+                ldiag[i-k]=true;
+                rdiag[i+k]=true;
                 pass(ans,a,i+1);
                 a[i][k]='.';
                 col[k]=false;
+                 ldiag[i-k]=false;
+                rdiag[i+k]=false;
             }
         }
 
