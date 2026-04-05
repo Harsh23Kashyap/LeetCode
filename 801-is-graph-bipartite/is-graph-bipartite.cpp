74bcb16d62911abdafa71bdbb5d136ada9eb7a1e
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-04-05 21:12:58 +0530

    Time: 8 ms (5.63%) | Memory: 21 MB (5.16%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
+        unordered_map<int,vector<int>> u;
         int n=graph.size();
-        vector<int> vis(n,-1);
-
-
-        queue<int> q;
-        // q.push(0);
+        for(int i=0;i<graph.size();i++){
+            for(int j=0;j<graph[i].size();j++){
+                u[i].push_back(graph[i][j]);
+            }
+        }
+        vector<int> col(n,-1);
 
         for(int i=0;i<n;i++){
-            if(vis[i]!=-1){
+            if(col[i]!=-1)
                 continue;
+            
 
-            }
+            queue<int> q;
             q.push(i);
-            int col=0;
-            vis[col]=0;
-
+            col[i]=1;
             while(!q.empty()){
                 auto it=q.front();
-                int mycol=vis[it];
                 q.pop();
-                for(auto ne:graph[it]){
-                    
-                    if(vis[ne]==-1){
-                        vis[ne]=(mycol==0)?1:0;
+                int currcolor=col[it];
+
+                for(auto ne:u[it]){
+                    if(col[ne]!=-1 and col[ne]!=currcolor)
+                    continue;
+                    if(col[ne]==currcolor)
+                    return false;
+                    if(col[ne]==-1){
+                        col[ne]=((currcolor==1)?2:1);
                         q.push(ne);
-                    }
-                    else{
-                        if(vis[ne]==mycol){
-                            return false;;
-                        }
-                        
                     }
                 }
             }
+          
         }
-        return true;
+          return true;
     }
 };
