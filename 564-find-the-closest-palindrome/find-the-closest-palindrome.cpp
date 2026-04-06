6a1ee60ef4dc95b0433d21562331c35b2fca26c9
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-04-06 23:58:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (22.99%) - LeetSync

--- 564-find-the-closest-palindrome/find-the-closest-palindrome.cpp
+++ 564-find-the-closest-palindrome/find-the-closest-palindrome.cpp
@@ -1,47 +1,63 @@
 class Solution {
 public:
     string nearestPalindromic(string n) {
-        if(n.size()==1){
+         if(n.size()==1){
             if(n=="0")
                 return "1";
             return to_string(n[0]-'0'-1);
         }
-        long long num = stoll(n);
-        int len = n.size();
 
-        // Edge candidates
-        vector<long long> candidates;
-        candidates.push_back(stoll(string(len - 1, '9')));
-        candidates.push_back(stoll("1" + string(len - 1, '0') + "1"));
+        long long no=stoll(n);
+        int sz=n.size();
+        string n99s(sz,'9');
+        string n9s(sz-1,'9');
+        long long n99=stoll(n99s);
+        long long n9=stoll(n9s);
+        string zero(sz-2,'0');
+        long long n100=stoll("1"+zero+"1");
+        string zero1(sz-1,'0');
+        long long n1000=stoll("1"+zero1+"1");
 
-        // Get prefix
-        long long prefix = stoll(n.substr(0, (len + 1) / 2));
 
-        // Generate 3 palindromes from prefix-1, prefix, prefix+1
-        for (long long i = prefix - 1; i <= prefix + 1; i++) {
-            string left = to_string(i);
-            string right = left;
+        vector<long long> ans;
+        ans.push_back(n99);
+        ans.push_back(n9);
+        ans.push_back(n100);
+        ans.push_back(n1000);
+        int half=sz/2;
+        if(sz%2!=0)
+            half++;
 
-            if (len % 2 == 1) right.pop_back(); // remove middle for odd
+        long long prefix=stoll(n.substr(0,half));
 
-            reverse(right.begin(), right.end());
-            string pal = left + right;
-
-            candidates.push_back(stoll(pal));
+        for(int i=prefix-1;i<=prefix+1;i++){
+            string left=to_string(i);
+            string right=left;
+            if(sz%2!=0)
+                right.pop_back();
+            reverse(right.begin(),right.end());
+            ans.push_back(stoll(left+right));
         }
 
-        long long best = -1;
-
-        for (auto cand : candidates) {
-            if (cand == num) continue;
-
-            if (best == -1 ||
-                llabs(cand - num) < llabs(best - num) ||
-                (llabs(cand - num) == llabs(best - num) && cand < best)) {
-                best = cand;
+        long long best = LLONG_MAX, diff = LLONG_MAX;
+        for(auto it : ans){
+            cout<<it<<endl;
+            if(it==no)
+                continue;
+            long long currDiff = llabs(it - no);
+            if(currDiff < diff){
+                diff = currDiff;
+                best = it;
+            }
+            else if(currDiff == diff){
+                best = min(best, it);
             }
         }
 
         return to_string(best);
+
+
+
+
     }
 };
