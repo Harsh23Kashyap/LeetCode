707a7ba55e7de8732ae3b1037a40f34bfbb2d5bd
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-04-08 01:37:45 +0530

    Time: 15 ms (44.16%) | Memory: 20.9 MB (51.07%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -1,37 +1,47 @@
 class Solution {
 public:
 vector<int> dx={-1,0,1,0,-1};
-int maxi=1;
-vector<vector<int>> dp;
-int trya(int r, int c, int i, int j, vector<vector<int>>& m)
-{
+    int dfs(int i, int j, vector<vector<int>>& matrix, vector<vector<int>> &dp){
+        
 
-    if(dp[i][j]!=-1)
-    return dp[i][j];
-    int curr=0;
-    for(int k=0;k<4;k++){
-        int nx=i+dx[k],ny=j+dx[k+1];
-        if(nx>=r or ny>=c or nx<0 or ny<0)
-        continue;
-        if(m[nx][ny]<=m[i][j])
-        continue;
-        curr=max(curr,trya(r,c,nx,ny,m));
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+
+        int curr=matrix[i][j];
+        matrix[i][j]=INT_MIN;
+        int bp=0;
+        for(int k=0;k<4;k++)
+        {
+            int nx=i+dx[k],ny=j+dx[k+1];
+
+            if(nx<0 or ny<0 or nx>=matrix.size() or ny>=matrix[0].size())
+                continue;
+
+            if(matrix[nx][ny]>curr){
+                // cout<<"Going to "<<nx<<" "<<ny<<endl;
+                bp=max(bp,dfs(nx,ny,matrix,dp));
+            }
+
+        }
+        matrix[i][j]=curr;
+        // cout<<" from "<<i<<" "<<j<<" we got "<<1+bp<<endl;
+        return dp[i][j]=1+bp;
+
     }
-    dp[i][j]=max(dp[i][j],1+curr);
-
-    maxi=max(maxi,dp[i][j]);
-    return dp[i][j];
-
-}
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         int m=matrix.size(),n=matrix[0].size();
-        vector<vector<int>> dpp(m,vector<int> (n,-1));
-        dp=dpp;
+        int bp=0;
+
+ vector<vector<int>> dp(m, vector<int> (n,-1));
+
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                trya(m,n,i,j,matrix);
+                if(dp[i][j]!=INT_MAX){
+                    bp=max(bp,dfs(i,j,matrix,dp));
+                    // cout<<i<<" "<<j<<" "<<bp<<endl;
+                }
             }
         }
-        return maxi;
+        return bp;
     }
 };
