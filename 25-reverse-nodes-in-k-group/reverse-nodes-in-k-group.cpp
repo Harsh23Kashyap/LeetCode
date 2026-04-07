efaa0ff92d96e1822277a72376705af60dbced15
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-04-07 15:13:05 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (69.83%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -1,62 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* reverse(ListNode* head, int k){
-        cout << "\n=== New call to reverse ===\n";
-
-        ListNode* curr = head;
-        ListNode* nhead = curr;
-        int c = 0;
-
-        cout << "Counting nodes from current head...\n";
-        while(nhead != NULL and c<k){
-            cout << nhead->val << " -> ";
-            nhead = nhead->next;
+ListNode* reverse(ListNode* head, int k){
+        ListNode* curr=head;
+        ListNode* nhead=curr;
+        int c=0;
+        while(nhead!=NULL and c<k){
+            nhead=nhead->next;
             c++;
         }
-        cout << "NULL\n";
-        cout << "Total nodes count = " << c << "\n";
-
-        if(c < k){
-            cout << "Less than k (" << k << "), returning head as-is\n";
+        if(c<k)
             return curr;
+
+        
+        ListNode* prev=NULL; 
+        while(curr!=nhead){
+            ListNode* nex=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nex;
         }
-
-        cout << "Reversing first (" << k << "), nodes...\n";
-
-        ListNode* prev = NULL; 
-        curr = head;
-        int step = 0;
-
-        // ⚠️ Note: your condition uses nhead (currently NULL)
-        while(curr != nhead){
-            ListNode* nex = curr->next;
-
-            cout << "Step " << step++ << ": reversing node " << curr->val << "\n";
-
-            curr->next = prev;
-            prev = curr;
-            curr = nex;
-
-            if(curr == NULL){
-                cout << "Reached end during reversal\n";
-            }
-        }
-
-        cout << "Finished reversing segment. New head = " << (prev ? prev->val : -1) << "\n";
-        cout << "Old head (now tail) = " << head->val << "\n";
-
-        cout << "Recursing for next segment...\n";
-        head->next = reverse(nhead, k);
-
+        head->next=reverse(nhead,k);
         return prev;
-    }
 
+}
     ListNode* reverseKGroup(ListNode* head, int k) {
-        cout << "Starting reverseKGroup with k = " << k << "\n";
-
-        ListNode* del = new ListNode(-1);
-        del->next = head;
-
-        return reverse(head, k);
+        ListNode* del=new ListNode(-1);
+        del->next=head;
+        return reverse(head,k);
+        
     }   
 };
