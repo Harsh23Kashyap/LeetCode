1f7db7ffc3474756a3d03b011e29ec10272c12a6
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-04-07 20:30:26 +0530

    Time: 562 ms (20.83%) | Memory: 317 MB (9.21%) - LeetSync

--- 3105-minimum-edge-reversals-so-every-node-is-reachable/minimum-edge-reversals-so-every-node-is-reachable.cpp
+++ 3105-minimum-edge-reversals-so-every-node-is-reachable/minimum-edge-reversals-so-every-node-is-reachable.cpp
@@ -2,106 +2,61 @@ class Solution {
 public:
 int ans=0;
 unordered_set<int> vis;
+bool dfs( unordered_map<int,vector<pair<int,int>>> &u, int node, int n){
+    if(vis.size()==n)
+    return true;
 
-void dfs(unordered_map<int,vector<pair<int,int>>> &u, int node, int n){
-    // cout << "DFS at node: " << node << ", vis size: " << vis.size() << ", ans: " << ans << "\n";
-
-    if(vis.size()==n){
-        // cout << "All nodes visited in DFS, returning\n";
-        return;
-    }
-
+    //i wanna go next;
     for(auto it:u[node]){
-        // cout << "  Exploring edge " << node << " -> " << it.first 
-        //      << " with cost: " << it.second << "\n";
-
         if(vis.find(it.first)==vis.end()){
-            // cout << "    Visiting new node: " << it.first << "\n";
-
             vis.insert(it.first);
             ans+=it.second;
+           if(dfs(u,it.first,n))
+           return true;
+        }
 
-            // cout << "    Updated ans: " << ans << "\n";
-
-            dfs(u,it.first,n);
-        } 
     }
+    return false;
+
 }
+    vector<int> minEdgeReversals(int n, vector<vector<int>>& edges) {
+        /*This is the logic is quite simple when you get it. The logic is basically that we first give the value to each edge. For every edge that is in correct order (that is, from u to v), we give it a value of 0 because we don't need to reverse it. For every other edge that is in the opposite direction, we give it a cost of 1 so that we can clearly differentiate between the opposite edge.
 
-vector<int> minEdgeReversals(int n, vector<vector<int>>& edges) {
+Once we have that, we start from any node. Let's say we start from node 0, because we want to find the answer for 0 first. We find all the edges that are opposite and that we have calculated for 0 completely. What we then basically do is we just do a sort of BFS traversal where we look into what other child of 0 we will get; we will basically get one child only because it's sort of a tree. We go there and we see, "Okay, going to this edge, did I have to reverse the edge? If yes, then otherwise the cost will increase by 1." We keep on doing this for all the limits. 
+        */
+        unordered_map<int,vector<pair<int,int>>> u;
 
-    unordered_map<int,vector<pair<int,int>>> u;
-
-    // cout << "Building graph...\n";
-    for(auto it:edges)
-    {
-        // cout << "Edge: " << it[0] << " -> " << it[1] << "\n";
-
-        u[it[0]].push_back({it[1],0});
-        u[it[1]].push_back({it[0],1});
-    }
-
-    // // cout << "\nAdj list:\n";
-    // for(auto &p : u){
-    //     // cout << p.first << ": ";
-    //     // for(auto &ne : p.second){
-    //     //     // cout << "(" << ne.first << "," << ne.second << ") ";
-    //     // }
-    //     // // cout << "\n";
-    // }
-
-    // cout << "\n--- DFS START ---\n";
-    vis.insert(0);
-    dfs(u,0,n);
-    // cout << "--- DFS END --- ans = " << ans << "\n\n";
-
-    vis.clear();
-    vector<int> res(n,0);
-    res[0]=ans;
-
-    queue<int> q;
-    q.push(0);
-    vis.insert(0);
-
-    // cout << "--- BFS START ---\n";
-
-    while(!q.empty()){
-        auto it=q.front();
-        q.pop();
-
-        // cout << "\nProcessing node: " << it << ", current ans: " << ans << "\n";
-
-        for(auto ne:u[it]){
-            // cout << "  Checking neighbor: " << ne.first 
-            //      << " cost: " << ne.second << "\n";
-
-            if(vis.find(ne.first)!=vis.end()){
-                // cout << "    Already visited, skipping\n";
+        for(auto it:edges)
+        {
+            u[it[0]].push_back({it[1],0});
+            u[it[1]].push_back({it[0],1});
+        }
+ 
+        //lets calculate for 0
+        vis.insert(0);
+        dfs(u,0,n);
+        vis.clear();
+        vector<int> res(n,0);
+        res[0]=ans;
+        queue<int> q;
+        q.push(0);
+        vis.insert(0);
+        while(!q.empty()){
+            auto it=q.front();
+            q.pop();
+            for(auto ne:u[it]){
+                if(vis.find(ne.first)!=vis.end())
                 continue;
+                ans=(ne.second==1)?res[it]-1:res[it]+1;
+                vis.insert(ne.first);
+                res[ne.first]=ans;
+                q.push(ne.first);
+                
             }
 
-            int old_ans = ans;
-            ans=(ne.second==1)?res[it]-1:res[it]+1;
-
-            // cout << "    ans updated from " << old_ans << " -> " << ans << "\n";
-
-            res[ne.first]=ans;
-            // cout << "    res[" << ne.first << "] = " << ans << "\n";
-
-            q.push(ne.first);
-            vis.insert(ne.first);
-
-            // cout << "    pushed " << ne.first << " to queue\n";
         }
+        return res;
+
+
     }
-
-    // cout << "\n--- BFS END ---\n";
-
-    // cout << "\nFinal result:\n";
-    // for(int i=0;i<n;i++){
-    //     cout << "res[" << i << "] = " << res[i] << "\n";
-    // }
-
-    return res;
-}
 };
