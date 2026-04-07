b272972196f608fafc0386c704b9e9aa08a4a3d3
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-04-07 19:59:24 +0530

    Time: 22 ms (52.73%) | Memory: 18.1 MB (92.16%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,45 +1,38 @@
 class Solution {
 public:
-    vector<int> dx = {0, 0, 1, -1};
-    vector<int> dy = {1, -1, 0, 0};
-
+typedef pair<int,pair<int,int>> pp;
+    vector<int> dx={0,0,1,-1};
+    vector<int> dy={1,-1,0,0};
+    
     int minCost(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
+        int m=grid.size(),n=grid[0].size();
+        priority_queue<pp,vector<pp>, greater<pp>> pq;
+        vector<vector<int>> dp(m, vector<int> (n,INT_MAX));
+        pq.push({0,{0,0}});
+        dp[0][0]=0;
 
-        priority_queue<
-            pair<int, pair<int,int>>,
-            vector<pair<int, pair<int,int>>>,
-            greater<>
-        > pq;
-
-        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
-
-        pq.push({0, {0,0}});
-        dist[0][0] = 0;
-
-        while(!pq.empty()) {
-            auto [cost, pos] = pq.top();
-            auto [x, y] = pos;
+        while(!pq.empty()){
+            auto [cost,dir]=pq.top();
+            int x=dir.first,y=dir.second;
             pq.pop();
 
-            // important optimization
-            if(cost > dist[x][y]) continue;
+            if(cost>dp[x][y])
+                continue;
 
-            for(int k = 0; k < 4; k++) {
-                int nx = x + dx[k];
-                int ny = y + dy[k];
+            
+            for(int k=1;k<=4;k++){
 
-                if(nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
-
-                int newCost = cost + ((k == grid[x][y] - 1) ? 0 : 1);
-
-                if(newCost < dist[nx][ny]) {
-                    dist[nx][ny] = newCost;
-                    pq.push({newCost, {nx, ny}});
+                int nc=(grid[x][y]==k)?0:1;
+                int nx=dx[k-1]+x,ny=dy[k-1]+y;
+                if(nx<0 or ny<0 or nx>=m or ny>=n)
+                    continue;
+                if(nc+cost<dp[nx][ny]){
+                    dp[nx][ny]=nc+cost;
+                    pq.push({nc+cost,{nx,ny}});
                 }
             }
         }
+        return dp[m-1][n-1];
 
-        return dist[m-1][n-1];
     }
 };
