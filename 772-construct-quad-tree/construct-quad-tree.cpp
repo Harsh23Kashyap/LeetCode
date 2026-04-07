fa167817209b875e336cf11b8e3821cf55e6d9f5
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-04-07 14:16:37 +0530

    Time: 9 ms (65.42%) | Memory: 19 MB (52.15%) - LeetSync

--- 772-construct-quad-tree/construct-quad-tree.cpp
+++ 772-construct-quad-tree/construct-quad-tree.cpp
@@ -1,34 +1,70 @@
+/*
+// Definition for a QuadTree node.
+class Node {
+public:
+    bool val;
+    bool isLeaf;
+    Node* topLeft;
+    Node* topRight;
+    Node* bottomLeft;
+    Node* bottomRight;
+    
+    Node() {
+        val = false;
+        isLeaf = false;
+        topLeft = NULL;
+        topRight = NULL;
+        bottomLeft = NULL;
+        bottomRight = NULL;
+    }
+    
+    Node(bool _val, bool _isLeaf) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = NULL;
+        topRight = NULL;
+        bottomLeft = NULL;
+        bottomRight = NULL;
+    }
+    
+    Node(bool _val, bool _isLeaf, Node* _topLeft, Node* _topRight, Node* _bottomLeft, Node* _bottomRight) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = _topLeft;
+        topRight = _topRight;
+        bottomLeft = _bottomLeft;
+        bottomRight = _bottomRight;
+    }
+};
+*/
+
 class Solution {
 public:
-    Node* build(vector<vector<int>>& grid, int r, int c, int size) { 
-        bool same = true;
-        int val = grid[r][c];
-
-        for (int i = r; i < r + size; i++) {
-            for (int j = c; j < c + size; j++) {
-                if (grid[i][j] != val) {
-                    same = false;
+    Node* build(vector<vector<int>>& grid, int i, int j, int sz){
+        bool same=true;
+        int val=grid[i][j];
+        for(int r=i;r<i+sz;r++){
+             for(int c=j;c<j+sz;c++){
+                if(grid[r][c]!=val){
+                    same=false;
                     break;
                 }
-            }
-            if (!same) break;
+            
         }
- 
-        if (same) 
-            return new Node(val, true);
-        
- 
-        int half = size / 2;
+        if(!same)
+        break;
+        }
 
-        Node* topLeft = build(grid, r, c, half);
-        Node* topRight = build(grid, r, c + half, half);
-        Node* bottomLeft = build(grid, r + half, c, half);
-        Node* bottomRight = build(grid, r + half, c + half, half);
-
-        return new Node(1, false, topLeft, topRight, bottomLeft, bottomRight);
+        if(same)
+          return new Node(val, true);
+        int half=sz/2;
+        Node* tl=build(grid,i,j,half);
+        Node* tr=build(grid,i,j+half,half);
+        Node* bl=build(grid,i+half,j,half);
+        Node* br=build(grid,i+half,j+half,half);
+        return new Node(1, false, tl,tr,bl,br);
     }
-
     Node* construct(vector<vector<int>>& grid) {
-        return build(grid, 0, 0, grid.size());
+        return build(grid,0,0,grid.size());
     }
 };
