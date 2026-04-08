99af2a56e5b5090949cca0a7ca998c7681240979
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-04-08 11:43:26 +0530

    Time: 365 ms (52.16%) | Memory: 46.7 MB (62.95%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
-typedef pair<int,pair<int,int>> pp;
-    struct cmp{
-
-        size_t operator()(const pair<int,int> &a) const{
-            size_t f=a.first;
-            size_t s=a.second;
-            return (f<<16)^s;
-        }
-    };
     int minCostConnectPoints(vector<vector<int>>& points) {
-        unordered_map<pair<int,int>,vector<pair<pair<int,int>,double>>, cmp> u;
-        int n=points.size();
-        for(int i=0;i<points.size();i++){
-            for(int j=i+1;j<points.size();j++){
-                int dist=abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
-                u[{points[i][0],points[i][1]}].push_back({{points[j][0],points[j][1]},dist});
-                u[{points[j][0],points[j][1]}].push_back({{points[i][0],points[i][1]},dist});
-            }
-        }
-
-        priority_queue<pp,vector<pp>, greater<pp>> pq;
-        int tc=0;
-        unordered_set<pair<int,int>,cmp> vis;
-        pq.push({0,{points[0][0],points[0][1]}});
+        int n = points.size();
         
-        while(!pq.empty()){
-            auto j=pq.top();
+        vector<int> minDist(n, INT_MAX);
+        vector<int> vis(n, 0);
+
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+        
+        pq.push({0, 0});  
+        minDist[0] = 0;
+
+        int totalCost = 0;
+
+        while (!pq.empty()) {
+            auto [cost, u] = pq.top();
             pq.pop();
 
-            if(vis.find(j.second)!=vis.end())
-            continue;
-            vis.insert(j.second);
+            if (vis[u]) 
+                continue;
 
-            tc+=j.first;
-            for(auto nn:u[j.second]){
-                pq.push({nn.second,nn.first});
+            vis[u] = 1;
+            totalCost += cost;
+
+            for (int v = 0; v < n; v++) {
+                if (!vis[v]) {
+                    int dist = abs(points[u][0] - points[v][0]) +
+                               abs(points[u][1] - points[v][1]);
+
+                    // if (dist < minDist[v]) {
+                    //     minDist[v] = dist;
+                        pq.push({dist, v});
+                    // }
+                }
             }
         }
-        return tc;
+
+        return totalCost;
     }
 };
