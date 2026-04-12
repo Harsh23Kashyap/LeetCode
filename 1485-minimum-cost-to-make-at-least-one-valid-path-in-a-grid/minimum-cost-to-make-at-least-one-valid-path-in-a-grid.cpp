eb3e97858ba20da99e71fac406e9bb72aba5f19a
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-04-12 23:04:29 +0530

    Time: 26 ms (44.04%) | Memory: 18.2 MB (92.40%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
-typedef pair<int,pair<int,int>> pp;
-    vector<int> dx={0,0,1,-1};
-    vector<int> dy={1,-1,0,0};
-    
+vector<int> dx={0,0,1,-1};
+vector<int> dy={1,-1,0,0};
+typedef pair<int, pair<int,int>> pp;
     int minCost(vector<vector<int>>& grid) {
-        int m=grid.size(),n=grid[0].size();
-        priority_queue<pp,vector<pp>, greater<pp>> pq;
-        vector<vector<int>> dp(m, vector<int> (n,INT_MAX));
+        
+
+        priority_queue<pp, vector<pp>, greater<pp>> pq;
+        vector<vector<int>> dp(grid.size(), vector<int> (grid[0].size(),INT_MAX));
         pq.push({0,{0,0}});
         dp[0][0]=0;
 
         while(!pq.empty()){
-            auto [cost,dir]=pq.top();
-            int x=dir.first,y=dir.second;
+            auto [cost,dim]=pq.top();
+            auto [x,y]=dim;
+            // cout<<x<<" "<<y<<endl;
             pq.pop();
-
-            if(cost>dp[x][y])
+            if(dp[x][y]<cost)
                 continue;
-
             
             for(int k=1;k<=4;k++){
-
-                int nc=(grid[x][y]==k)?0:1;
-                int nx=dx[k-1]+x,ny=dy[k-1]+y;
-                if(nx<0 or ny<0 or nx>=m or ny>=n)
+                int currcost=(grid[x][y]==k)?0:1;
+                int nx=dx[k-1]+x;
+                int ny=dy[k-1]+y;
+                if(not(nx>=0 and ny>=0 and nx<grid.size() and ny<grid[0].size()))
                     continue;
-                if(nc+cost<dp[nx][ny]){
-                    dp[nx][ny]=nc+cost;
-                    pq.push({nc+cost,{nx,ny}});
+                if(dp[nx][ny]>cost+currcost){
+                    dp[nx][ny]=cost+currcost;
+                    pq.push({cost+currcost,{nx,ny}});
                 }
             }
+
+            
         }
-        return dp[m-1][n-1];
+        return dp[grid.size()-1][grid[0].size()-1];
 
     }
 };
