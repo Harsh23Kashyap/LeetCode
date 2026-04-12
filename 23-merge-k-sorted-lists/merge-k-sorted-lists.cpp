fc0d8c5f6acc45794f05ad9e88a155a1c69228ba
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-04-12 23:37:21 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (81.16%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -10,33 +10,32 @@
  */
 class Solution {
 public:
-    struct cmp{
-        bool operator()(ListNode* a, ListNode* b){
-            return a->val>b->val;
-        }
-    };
+struct cmp{
+    static bool operator()(ListNode* a, ListNode* b){
+        return a->val>b->val;
+    }
+};
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode*,vector<ListNode*> , cmp> pq;
-        for(int i=0;i<lists.size();i++){
-            ListNode* start=lists[i];
-            if(start!=NULL)
-             pq.push(start);
-        }
-        
-        ListNode* neg=new ListNode(-1);
-        ListNode* head=neg;
+        priority_queue<ListNode*, vector<ListNode*> , cmp> pq;
 
+        for(auto it:lists){
+            if(it!=NULL)
+                pq.push(it);
+        }
+
+        ListNode* neg=new ListNode(-11);
+        ListNode* head=neg;
         while(!pq.empty()){
             auto it=pq.top();
             pq.pop();
-            if(it!=NULL and it->next!=NULL)
+
+            if(it->next!=NULL)
                 pq.push(it->next);
-            
-            neg->next=it;
-            neg=neg->next;
+
+            head->next=it;
+            head=head->next;
 
         }
-        return head->next;
-        
+        return neg->next;
     }
 };
