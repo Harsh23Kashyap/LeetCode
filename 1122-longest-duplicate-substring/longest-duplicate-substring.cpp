da031646fdc099505b45f9731f7b71efec7d643a
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-04-13 00:48:49 +0530

    Time: 1189 ms (53.20%) | Memory: 428.8 MB (23.80%) - LeetSync

--- 1122-longest-duplicate-substring/longest-duplicate-substring.cpp
+++ 1122-longest-duplicate-substring/longest-duplicate-substring.cpp
@@ -1,66 +1,66 @@
 class Solution {
 public:
-string s;
-int mod=1e9+7;
-int n;
-int base=31;
+    string s;
+    int n;
+    const long long mod = 1e9 + 7;
+    const int base = 31;
 
-    string check(int m){
+string check(int len) {
+    unordered_map<long long, vector<int>> mp;
 
-        unordered_map<long long, vector<int>> mp;
+    long long hash = 0, power = 1;
 
-        long long hash=0,power=1;
-
-        //window1;
-        for(int i=0;i<m;i++){
-            hash=(hash*base + (s[i]-'a'+1))%mod;
-            if(i<m-1)
-                power=(power*base)%mod;
-        }
-
-
-        mp[hash].push_back(0);
-
-        for(int i=m;i<s.size();i++){
-            hash=(hash-(s[i-m]-'a'+1)*power % mod + mod)%mod;
-
-             hash=(hash*base + (s[i]-'a'+1))%mod;
-
-             int start=i-m+1;
-
-             if(mp.count(hash)){
-                for(int prevStart:mp[hash]){
-                    if(s.substr(prevStart,m)==s.substr(start,m)){
-                        return s.substr(prevStart,m);
-                    }
-                }
-             }
-
-             mp[hash].push_back(start);
-
-
-
-
-        }
-
-        return "";
-
+    // first window
+    for (int i = 0; i < len; i++) {
+        hash = (hash * base + (s[i] - 'a' + 1)) % mod;
+        if (i < len - 1)
+            power = (power * base) % mod;
     }
-    string longestDupSubstring(string str) {
-        s=str;
-        int l=1, r=s.size();
-         n=s.size();
-        string ans="";
-        while(l<=r){
-            int m=l+(r-l)/2;
-             string cans=check(m);
-            if(cans=="")
-                r=m-1;
-            else{
-                l=m+1;
-                ans=cans;
+
+    mp[hash].push_back(0);
+
+    for (int i = len; i < n; i++) {
+        // remove left char
+        hash = (hash - (s[i - len] - 'a' + 1) * power % mod + mod) % mod;
+
+        // add new char
+        hash = (hash * base + (s[i] - 'a' + 1)) % mod;
+
+        int start = i - len + 1;
+
+        if (mp.count(hash)) {
+            for (int prevStart : mp[hash]) {
+                if (s.substr(prevStart, len) == s.substr(start, len)) {
+                    return s.substr(start, len); // ✅ verified match
+                }
             }
         }
+
+        mp[hash].push_back(start);
+    }
+
+    return "";
+}
+    string longestDupSubstring(string _s) {
+        s = _s;
+        n = s.size();
+
+        int l = 1, r = n;
+        string ans = "";
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+
+            string res = check(mid);
+
+            if (res != "") {
+                ans = res;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
         return ans;
     }
 };
