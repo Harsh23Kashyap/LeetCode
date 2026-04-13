3d1107fa543ed5ac6354aa946c9e6fc411f87e01
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-04-13 14:50:34 +0530

    Time: 410 ms (16.78%) | Memory: 186.4 MB (17.11%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.cpp
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -1,86 +1,105 @@
 class Solution {
 public:
-    //up down left right
-    int dx[4] = {-1,1,0,0};
-    int dy[4] = {0,0,-1,1};
-    
-    //this is the code which calculates the area of different islands currently present in the grid
-    int func(int i, int j, vector<vector<int>>&grid, vector<vector<int>>&parent, int currparent){
-        queue<pair<int,int>>qu;
-        qu.push({i,j});
-        grid[i][j] = 0;
-        int currsize = 0;
-        
-        while(qu.empty()==false){
-            int i = qu.front().first;
-            int j = qu.front().second;
-            parent[i][j] = currparent;
-            qu.pop();
-            currsize++;
-            
-            for(int d = 0; d<4; d++){
-                int currx = i+dx[d];
-                int curry = j+dy[d];
-                
-                if(currx>=0 and curry>=0 and currx<grid.size() and curry<grid[0].size() and grid[currx][curry]==1){
-                    grid[currx][curry] = 0;
-                    qu.push({currx, curry});
-                }
+    vector<int> parent;
+    vector<int> rank;
+
+    int find(int x){
+        if(x==parent[x])
+            return x;
+        return parent[x]=find(parent[x]);
+    }
+
+    void unite(int a, int b){
+        int pa=find(a);
+        int pb=find(b);
+ 
+        if(pa==pb){ 
+            return;
+        }
+        else{
+            if(rank[pa]<=rank[pb]){ 
+                rank[pb]+=rank[pa];
+                parent[pa]=pb;
+            }
+            else{ 
+                rank[pa]+=rank[pb];
+                parent[pb]=pa;
             }
         }
-        
-        return currsize;
     }
-    
-    int largestIsland(vector<vector<int>>& grid) {
-        int ans = 0;
-        int currparent = 1;
-        
-        //this map stores the parent of island mapped with its area
-        unordered_map<int,int>mp;
-        
-        //we are using union find in a different way, we are assigning a number to an island using parent matrix
-        //different islands have different parents but all the cells of an island have the same parent number
-        vector<vector<int>>parent(grid.size(), vector<int>(grid[0].size()));
-        for(int i = 0; i<grid.size(); i++){
-            for(int j = 0; j<grid[0].size(); j++){
-                if(grid[i][j]){
-                    currparent++;
-                    int currsize = func(i, j, grid, parent, currparent);
-                    mp[currparent] = currsize;
-                    ans = max(ans, currsize);
-                }
+
+    vector<int> dx={-1,0,1,0,-1};
+
+    void pass(int i, int j, int m, int n, vector<vector<int>>& grid, unordered_set<int> &vis)
+    { 
+
+        for(int k=0;k<4;k++){
+            int nx=i+dx[k],ny=j+dx[k+1];
+
+            if(nx>=0 and ny>=0 and nx<m and ny<n and grid[nx][ny]==1 and vis.find(nx*n+ny)==vis.end()){
+                 
+
+                vis.insert(nx*n+ny);
+                unite(i*n+j,nx*n+ny);
+                pass(nx,ny,m,n,grid,vis);
             }
+        }
+    }
+
+    int largestIsland(vector<vector<int>>& grid) {
+        
+        int m=grid.size(),n=grid[0].size();
+
+        parent.resize(m*n);
+        rank.resize(m*n);
+ 
+        for(int i=0;i<m*n;i++){
+            rank[i]=0;
+            parent[i]=i;
+            int r=i/n,c=i%n;
+            if(grid[r][c]==1)
+                rank[i]=1;
         }
 
-        
-        //we are done with the marking of all the islands in the grid, now we will see how much max area we can obtain if we change 0 to 1
-        //if the 0 touches the boundary with the cells belonging to different parents then all these islands will accumulate to become a larger island
-        
-        for(int i = 0; i<grid.size(); i++){
-            for(int j = 0; j<grid[0].size(); j++){
-                if(parent[i][j]==0){
-                    
-                    unordered_set<int>currparents;
-                    int currans = 0;
-                    
-                    for(int d = 0; d<4; d++){
-                        int currx = i+dx[d];
-                        int curry = j+dy[d];
-                        
-                        if(currx>=0 and curry>=0 and currx<grid.size() and curry<grid[0].size() and parent[currx][curry]!=0){
-                            if(currparents.count(parent[currx][curry])==0){
-                                currans+=mp[parent[currx][curry]];
-                                currparents.insert(parent[currx][curry]);
-                            }
+        unordered_set<int> vis;
+ 
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1 and vis.find(i*n+j)==vis.end()){ 
+                    vis.insert(i*n+j);
+                    pass(i,j,m,n,grid,vis);
+                }
+            }
+        } 
+        int maxi=0;
+        bool possible=false; 
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==0){
+ 
+
+                    unordered_set<int> pl;
+
+                    for(int k=0;k<4;k++){
+                        int nx=i+dx[k],ny=j+dx[k+1];
+                        possible=true;
+                        if(nx>=0 and ny>=0 and nx<m and ny<n and grid[nx][ny]==1){
+                            int p = find(nx*n+ny); 
+                            pl.insert(p);
                         }
                     }
-                    currans++;
-                    ans = max(ans, currans);
+
+                    int curr=0;
+ 
+                    for(auto it:pl)
+                        curr+=rank[it];
+                    
+                    maxi=max(maxi,curr+1);
                 }
             }
         }
-        
-        return ans;
+ 
+
+        return possible?maxi:m*n;
     }
 };
