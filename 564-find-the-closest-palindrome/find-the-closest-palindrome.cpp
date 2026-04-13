b0355330c2a578309d285b9f8baef9e22e495f29
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-04-13 08:51:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (29.68%) - LeetSync

--- 564-find-the-closest-palindrome/find-the-closest-palindrome.cpp
+++ 564-find-the-closest-palindrome/find-the-closest-palindrome.cpp
@@ -14,22 +14,18 @@ public:
         candidates.push_back(stoll(string(len - 1, '9')));
         candidates.push_back(stoll(string(len , '9')));
         candidates.push_back(stoll("1" + string(len - 1, '0') + "1"));
-         candidates.push_back(stoll("1" + string(len - 2, '0') + "1"));
-
-        // Get prefix
+        candidates.push_back(stoll("1" + string(len - 2, '0') + "1"));
+ 
         long long prefix = stoll(n.substr(0, (len + 1) / 2));
-
-        // Generate 3 palindromes from prefix-1, prefix, prefix+1
+ 
         for (long long i = prefix - 1; i <= prefix + 1; i++) {
-            string left = to_string(i);
-            string right = left;
-
-            if (len % 2 == 1) right.pop_back(); // remove middle for odd
-
-            reverse(right.begin(), right.end());
-            string pal = left + right;
-
-            candidates.push_back(stoll(pal));
+            string left=to_string(i);
+            string right=left;
+            if(len%2!=0)
+            right.pop_back();
+            reverse(right.begin(),right.end());
+            string curr=left+right;
+            candidates.push_back(stoll(curr));
         }
 
         long long best = -1;
@@ -37,9 +33,8 @@ public:
         for (auto cand : candidates) {
             if (cand == num) continue;
 
-            if (best == -1 ||
-                llabs(cand - num) < llabs(best - num) ||
-                (llabs(cand - num) == llabs(best - num) && cand < best)) {
+            if (best == -1 or llabs(cand - num) < llabs(best - num) or 
+                (llabs(cand - num) == llabs(best - num) and cand < best)) {
                 best = cand;
             }
         }
