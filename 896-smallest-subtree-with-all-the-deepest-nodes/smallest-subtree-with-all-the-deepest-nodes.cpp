382f875d17a07ec72166d373e791b81d23100aa0
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-04-13 23:27:26 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (46.90%) - LeetSync

--- 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -1,21 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    pair<TreeNode*, int> dfs(TreeNode* root) {
-        if (!root) return {NULL, 0};
+    pair<int, TreeNode*> pass(TreeNode* root){
+        if(root==NULL)
+            return {0,NULL};
+        
+        if(root->left==NULL and root->right==NULL)
+            return {1, root};
 
-        auto L = dfs(root->left);
-        auto R = dfs(root->right);
+        auto [sl,le]=pass(root->left);
+        auto [sr,re]=pass(root->right);
 
-        if (L.second > R.second)
-            return {L.first, L.second + 1};
-        if (R.second > L.second)
-            return {R.first, R.second + 1};
+        if(sl==sr){
+            return {1+sl,root};
+        }
+        else if(sl>sr){
+            return {1+sl,le};
+        }
+        return {1+sr,re};
+        
 
-        // same depth → current node is LCA
-        return {root, L.second + 1};
     }
-
     TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-        return dfs(root).first;
+        auto it=pass(root);
+        return it.second;
     }
 };
