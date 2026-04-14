3c9be83d9a51900e130a945aad1fe552a5625409
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-04-14 10:04:09 +0530

    Time: 8 ms (35.16%) | Memory: 21.1 MB (9.08%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
-    bool canFinish(int n, vector<vector<int>>& prerequisites) {
-        unordered_map<int,vector<int>> u;
-        vector<int> indegree(n,0);
-        for(auto it:prerequisites){
+    bool canFinish(int n, vector<vector<int>>& deps) {
+    vector<int> ans;
+         vector<int> indegree(n,0);
+         unordered_map<int,vector<int>> u;
+
+         for(auto it:deps){
             u[it[1]].push_back(it[0]);
             indegree[it[0]]++;
-        }
 
-        int c=0;
-        queue<int> q;
+         }
 
-        for(int i=0;i<indegree.size();i++){
+         queue<int> q;
+         for(int i=0;i<indegree.size();i++){
             if(indegree[i]==0){
+             ans.push_back(i);
                 q.push(i);
-                c++;
             }
-        }
+         }
 
-        while(!q.empty()){
+         while(!q.empty()){
             auto it=q.front();
             q.pop();
-            for(auto nn:u[it]){
-                indegree[nn]--;
-                if(indegree[nn]==0){
-                    c++;
-                q.push(nn);
+            for(auto ne:u[it]){
+                if(--indegree[ne]==0){
+                q.push(ne);
+                ans.push_back(ne);
                 }
             }
-        }
-
-        return c==n;
+         }
+         if(ans.size()==n)
+            return true;
+            return false;
 
 
     }
