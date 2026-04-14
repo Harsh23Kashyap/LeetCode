b654df66207a88653fee455e6f70f9f3649b30c1
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-04-15 01:52:48 +0530

    Time: 172 ms (25.23%) | Memory: 140.9 MB (22.39%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.cpp
+++ 1397-search-suggestions-system/search-suggestions-system.cpp
@@ -19,7 +19,7 @@ public:
            if(curr->children.find(word[i]) == curr->children.end())
                 curr->children[word[i]]=new Trie(word[i],false);
           curr = curr->children[word[i]];
-            curr->words.push_back(word);
+          curr->words.push_back(word);
         }
         curr->possible=true;
     }
@@ -32,29 +32,32 @@ public:
          for(auto it:products)
             insertAt(it);
 
-         
+        
 
         Trie* curr=root;
         bool prefix=true;
-         for(int i=0;i<searchWord.size();i++){
-            if(prefix and curr->children.find(searchWord[i]) != curr->children.end()){
-            curr=curr->children[searchWord[i]];
-             vector<string> temp=curr->words;
-            sort(temp.begin(),temp.end());
-            // cout<<temp.size()<<endl;
-            if(temp.size()<=3)
-                ans.push_back(temp);
-            else
-                ans.push_back({temp[0],temp[1],temp[2]});
+         for(int i=0;i<searchWord.size();i++)
+         {
+            vector<string> currans;
+            char ch=searchWord[i];
+            if(!prefix or !curr->children.count(ch)){
+                prefix=false;
+                ans.push_back(currans);
+                continue;
 
             }
-            else{
-                prefix=false;
-                ans.push_back({});
-                continue;
+
+            curr=curr->children[(ch)];
+            vector<string> all=curr->words;
+            sort(all.begin(),all.end());
+            if(all.size()>3){
+                currans.insert(currans.end(),all.begin(), all.begin()+3);
+                ans.push_back(currans);
             }
-           
-            // ans.push_back(curr->words);
+            else
+                ans.push_back(all);
+
+
          }
          return ans;
     }
