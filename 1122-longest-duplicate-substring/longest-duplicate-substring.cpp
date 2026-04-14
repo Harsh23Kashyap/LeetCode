ec5e4e7644da8f691c2cedbe7fa356254e7f1bfd
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-04-14 08:47:44 +0530

    Time: 1224 ms (41.60%) | Memory: 428.7 MB (25.80%) - LeetSync

--- 1122-longest-duplicate-substring/longest-duplicate-substring.cpp
+++ 1122-longest-duplicate-substring/longest-duplicate-substring.cpp
@@ -1,66 +1,62 @@
 class Solution {
 public:
-    string s;
-    int n;
-    const long long mod = 1e9 + 7;
-    const int base = 31;
+string s;
+int n;
 
-string check(int len) {
-    unordered_map<long long, vector<int>> mp;
+int base=31;
+int mod=1e9+7;
+    string possible(int m){
+        unordered_map<int, vector<int>> u;
+        long long hash=0,power=1;
+        for(int i=0;i<m;i++){
+            hash=(hash*base+(s[i]-'a'+1))%mod;
+            if(i<m-1){
+                power=(power*base)%mod;
+            }
+        }
 
-    long long hash = 0, power = 1;
+        u[hash].push_back(0);
 
-    // first window
-    for (int i = 0; i < len; i++) {
-        hash = (hash * base + (s[i] - 'a' + 1)) % mod;
-        if (i < len - 1)
-            power = (power * base) % mod;
-    }
+        for(int j=m;j<s.size();j++){
+            //remove
+            hash=(hash-((s[j-m]-'a'+1)*power% mod )+mod)%mod;
 
-    mp[hash].push_back(0);
+            //add
+            hash=(hash*base+ ((s[j]-'a'+1)))%mod;
 
-    for (int i = len; i < n; i++) {
-        // remove left char
-        hash = (hash - (s[i - len] - 'a' + 1) * power % mod + mod) % mod;
-
-        // add new char
-        hash = (hash * base + (s[i] - 'a' + 1)) % mod;
-
-        int start = i - len + 1;
-
-        if (mp.count(hash)) {
-            for (int prevStart : mp[hash]) {
-                if (s.substr(prevStart, len) == s.substr(start, len)) {
-                    return s.substr(start, len); // ✅ verified match
+            if(u.count(hash)  ){
+                for(auto it:u[hash]){
+                    if(s.substr(j-m+1,m)==s.substr(it,m))
+                    {
+                        return s.substr(it,m);
+                    }
                 }
+                
             }
-        }
+             u[hash].push_back(j-m+1);
 
-        mp[hash].push_back(start);
+        }
+        return "";
     }
 
-    return "";
-}
-    string longestDupSubstring(string _s) {
-        s = _s;
-        n = s.size();
+    string longestDupSubstring(string ns) {
+        s=ns;
+        n=s.size();
 
-        int l = 1, r = n;
-        string ans = "";
-
-        while (l <= r) {
-            int mid = (l + r) / 2;
-
-            string res = check(mid);
-
-            if (res != "") {
-                ans = res;
-                l = mid + 1;
-            } else {
-                r = mid - 1;
+        int l=1,r=n;
+        int ans=1;
+        string sans="";
+        while(l<=r){
+            int m=l+(r-l)/2;
+            string check=possible(m);
+            if(check!=""){
+                sans=check;
+                l=m+1;
             }
+            else
+            r=m-1;
         }
-
-        return ans;
+        return sans;
+        
     }
 };
