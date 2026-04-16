b1fcf1e16b3b3fd5cf03351dfb5d68696ef31e7b
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-04-16 10:05:21 +0530

    Time: 74 ms (20.81%) | Memory: 47.9 MB (19.79%) - LeetSync

--- 721-accounts-merge/accounts-merge.cpp
+++ 721-accounts-merge/accounts-merge.cpp
@@ -1,55 +1,63 @@
+class DSU {
+public:
+    unordered_map<string, string> parent;
+
+    string find(string x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    void unite(string a, string b) {
+        string pa = find(a);
+        string pb = find(b);
+        if (pa != pb) {
+            parent[pb] = pa;
+        }
+    }
+};
+
 class Solution {
 public:
-unordered_map<string,string> parent;
-string find(string x){
-    if(parent[x]==x)
-        return x;
-    return parent[x]=find(parent[x]);
-}
-void unite(string a, string b){
-    string pa=find(a);
-    string pb=find(b);
-    parent[pb]=pa;
-}
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        DSU dsu;
+        unordered_map<string, string> emailToName;
 
-        unordered_map<string, string> names;
-        for(int i=0;i<accounts.size();i++){
-            for(int j=1;j<accounts[i].size();j++){
-               parent[accounts[i][j]]=accounts[i][j];
+        // Step 1: Initialize DSU + map email -> name
+        for (auto& acc : accounts) 
+        {
+            string name = acc[0];
+            for (int i = 1; i < acc.size(); i++) 
+            {
+                string email = acc[i];
+                if (!dsu.parent.count(email)) {
+                    dsu.parent[email] = email;
+                }
+                emailToName[email] = name;
             }
         }
-
-        for(int i=0;i<accounts.size();i++){
-            string n=accounts[i][0];
-            string p=accounts[i][1];
-            names[p]=n; 
-            for(int j=2;j<accounts[i].size();j++){
-                unite(accounts[i][j],p);
-                names[accounts[i][j]]=n;
+ 
+        for (auto& acc : accounts) {
+            string firstEmail = acc[1];
+            for (int i = 2; i < acc.size(); i++) {
+                dsu.unite(firstEmail, acc[i]);
             }
         }
-
-        unordered_map<string, set<string>> mails;
-         for(int i=0;i<accounts.size();i++){
-            for(int j=1;j<accounts[i].size();j++){
-               string myprent=find(accounts[i][j]);
-               mails[myprent].insert(accounts[i][j]);
-            }
+ 
+        unordered_map<string, vector<string>> groups;
+        for (auto& [email, name] : emailToName) {
+            string parent = dsu.find(email);
+            groups[parent].push_back(email);
+        }
+ 
+        vector<vector<string>> res;
+        for (auto& [parent, emails] : groups) {
+            sort(emails.begin(), emails.end());
+            vector<string> temp;
+            temp.push_back(emailToName[parent]); // name
+            for (auto& e : emails) temp.push_back(e);
+            res.push_back(temp);
         }
 
-
-        vector<vector<string>> ans;
-        for(auto it:mails){
-            string n=names[it.first];
-            vector<string> curr;
-            curr.push_back(n);
-            for(auto nit:it.second)
-                curr.push_back(nit);
-            ans.push_back(curr);
-        }
-        return ans;
-
-        
+        return res;
     }
 };
