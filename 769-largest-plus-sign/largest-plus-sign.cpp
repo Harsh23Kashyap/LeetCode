5f3a9e3707a571b2a176a3759577ec2650126650
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-04-16 10:16:56 +0530

    Time: 200 ms (22.12%) | Memory: 38.5 MB (62.36%) - LeetSync

--- 769-largest-plus-sign/largest-plus-sign.cpp
+++ 769-largest-plus-sign/largest-plus-sign.cpp
@@ -1,53 +1,56 @@
 class Solution {
 public:
     int orderOfLargestPlusSign(int n, vector<vector<int>>& mines) {
-         
-        vector<vector<int>> grid(n, vector<int>(n, 1));
+        
+        unordered_set<int> st;
         for (auto &m : mines) {
-            grid[m[0]][m[1]] = 0;
+            st.insert(m[0] * n + m[1]);
         }
- 
-        vector<vector<int>> left(n, vector<int>(n, 0));
-        vector<vector<int>> right(n, vector<int>(n, 0));
-        vector<vector<int>> up(n, vector<int>(n, 0));
-        vector<vector<int>> down(n, vector<int>(n, 0));
- 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 0) left[i][j] = 0;
-                else left[i][j] = (j == 0 ? 1 : left[i][j-1] + 1);
-            }
-        }
- 
-        for (int i = 0; i < n; i++) {
-            for (int j = n-1; j >= 0; j--) {
-                if (grid[i][j] == 0) right[i][j] = 0;
-                else right[i][j] = (j == n-1 ? 1 : right[i][j+1] + 1);
-            }
-        }
- 
-        for (int j = 0; j < n; j++) {
-            for (int i = 0; i < n; i++) {
-                if (grid[i][j] == 0) up[i][j] = 0;
-                else up[i][j] = (i == 0 ? 1 : up[i-1][j] + 1);
-            }
-        }
- 
-        for (int j = 0; j < n; j++) {
-            for (int i = n-1; i >= 0; i--) {
-                if (grid[i][j] == 0) down[i][j] = 0;
-                else down[i][j] = (i == n-1 ? 1 : down[i+1][j] + 1);
-            }
-        }
- 
-        int ans = 0;
 
+        vector<vector<int>> dp(n, vector<int>(n, n));
+
+        // LEFT
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (st.count(i*n + j)) count = 0;
+                else count++;
+                dp[i][j] = min(dp[i][j], count);
+            }
+
+            // RIGHT
+            count = 0;
+            for (int j = n-1; j >= 0; j--) {
+                if (st.count(i*n + j)) count = 0;
+                else count++;
+                dp[i][j] = min(dp[i][j], count);
+            }
+        }
+
+        // UP & DOWN
+        for (int j = 0; j < n; j++) {
+            int count = 0;
+
+            // UP
+            for (int i = 0; i < n; i++) {
+                if (st.count(i*n + j)) count = 0;
+                else count++;
+                dp[i][j] = min(dp[i][j], count);
+            }
+
+            // DOWN
+            count = 0;
+            for (int i = n-1; i >= 0; i--) {
+                if (st.count(i*n + j)) count = 0;
+                else count++;
+                dp[i][j] = min(dp[i][j], count);
+            }
+        }
+
+        int ans = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    int order = min({left[i][j], right[i][j], up[i][j], down[i][j]});
-                    ans = max(ans, order);
-                }
+                ans = max(ans, dp[i][j]);
             }
         }
 
