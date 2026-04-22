d04cf4c3ce4cd8b4e69a4e7e51d2c39f9f40d3cf
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-04-22 18:50:46 +0530

    Time: 1 ms (42.64%) | Memory: 69.6 MB (59.61%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        deque<int> dq;  // store indices of zeros
-        int left = 0;
-        int ans = 0;
+        int left = 0, zeroCount = 0, ans = 0;
 
-        for (int right = 0; right < nums.size(); right++) {
+        for (int right = 0; right < nums.size(); right++) 
+        {
+            if (nums[right] == 0) 
+                zeroCount++;
 
-            if (nums[right] == 0)
-                dq.push_back(right);
-
-            if (dq.size() > k) {
-                left = dq.front() + 1;
-                dq.pop_front();
+            while (left<=right and zeroCount > k) 
+            {
+                if (nums[left] == 0) 
+                    zeroCount--;
+                left++;
             }
 
             ans = max(ans, right - left + 1);
