b520d29d935693a15c4996cd3a2f91da0f2cacad
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-04-25 15:07:16 +0530

    Time: 157 ms (21.35%) | Memory: 77.7 MB (11.24%) - LeetSync

--- 3781-maximize-the-distance-between-points-on-a-square/maximize-the-distance-between-points-on-a-square.cpp
+++ 3781-maximize-the-distance-between-points-on-a-square/maximize-the-distance-between-points-on-a-square.cpp
@@ -1,71 +1,75 @@
 class Solution {
 public:
+    vector<long long> pass(vector<vector<int>>& points,int side){
+        vector<long long> single;
+        for(auto it:points){
+            
+            if(it[1]==0)
+            single.push_back(it[0]);
+
+            else if(it[0]==side)
+                single.push_back(side+it[1]);
+            
+            else if(it[1]==side)
+                single.push_back(3ll*side-it[0]);
+            
+            else
+                single.push_back(4ll*side-it[1]);
+
+        }
+
+        sort(single.begin(),single.end());
+        int n=single.size();
+        for(int i=0;i<n;i++)
+            single.push_back(single[i]+side*4ll);
+
+        return single;
+    }
+
+    bool can(int d,vector<long long> &single, int k, int side ){
+
+        vector<long long> nxt(single.size());
+
+        for(int i=0;i<single.size();i++)
+            nxt[i]=lower_bound(single.begin(),single.end(),single[i]+d)-single.begin();
+        
+
+        for(int i=0;i<single.size()/2;i++){
+            int c=1;
+            long long curr=i;
+
+            for(int step=1;step<k;step++){
+                curr=nxt[curr];
+                if(curr>=i+single.size()/2){
+                    break;
+                }
+                c++;
+            }
+
+            if(c==k){
+                long long first = single[i];
+                long long last = single[curr];
+
+                if (4ll*side - (last - first) >= d)
+                    return true;
+            }
+        }
+        return false;
+    }
     int maxDistance(int side, vector<vector<int>>& points, int k) {
-        int n = points.size();
-        long long L = 4LL * side;
+        vector<long long> single=pass(points,side);
 
-        vector<long long> pos;
-        for (auto &p : points) {
-            long long x = p[0], y = p[1];
-
-            if (y == 0) pos.push_back(x);
-            else if (x == side) pos.push_back(side + y);
-            else if (y == side) pos.push_back(3LL * side - x);
-            else pos.push_back(4LL * side - y);
+        long long l=0,h=4ll*side;
+        int ans=0;
+        while(l<=h){
+            int m=l+(h-l)/2;
+            if(can(m,single,k,side)){
+                ans=m;
+                l=m+1;
+            }
+            else
+                h=m-1;
         }
-
-        sort(pos.begin(), pos.end());
-
-        // duplicate
-        vector<long long> ext = pos;
-        for (auto x : pos) ext.push_back(x + L);
-
-        int m = ext.size();
-
-        auto can = [&](long long D) {
-            vector<int> nxt(m);
-
-            // build next jump using binary search
-            for (int i = 0; i < m; i++) {
-                long long target = ext[i] + D;
-                nxt[i] = lower_bound(ext.begin(), ext.end(), target) - ext.begin();
-            }
-
-            // try each start
-            for (int i = 0; i < n; i++) {
-                int count = 1;
-                int cur = i;
-
-                for (int step = 1; step < k; step++) {
-                    cur = nxt[cur];
-                    if (cur >= i + n) break;
-                    count++;
-                }
-
-                if (count == k) {
-                    long long first = ext[i];
-                    long long last = ext[cur];
-
-                    if (L - (last - first) >= D)
-                        return true;
-                }
-            }
-
-            return false;
-        };
-
-        long long low = 0, high = L, ans = 0;
-
-        while (low <= high) {
-            long long mid = (low + high) / 2;
-            if (can(mid)) {
-                ans = mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-
-        return (int)ans;
+        return ans;
     }
 };
