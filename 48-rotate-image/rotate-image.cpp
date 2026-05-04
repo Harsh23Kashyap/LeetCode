04ba7b633fd7dea8ef140bb6795aa05bd0e55c53
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-05-04 13:45:53 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (92.70%) - LeetSync

---
+++ 48-rotate-image/rotate-image.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+         for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<=i;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(auto &it:matrix)
+            reverse(it.begin(),it.end());
+
+        
+       
+        
+    }
+};
