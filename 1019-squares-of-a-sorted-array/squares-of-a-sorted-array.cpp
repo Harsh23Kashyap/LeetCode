c1242bd881ab1be9725723b7e6597b71fe11ea45
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-05-06 01:25:12 +0530

    Time: 0 ms (100.00%) | Memory: 30.2 MB (86.51%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,41 +1,24 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int i=0,j=0;
-        while(i<nums.size() and nums[i]<0)
-        i++,j++;
-        j--;
-        vector<int> ans;
-        while(i<nums.size() and nums[i]==0){
-            ans.push_back(nums[i]);
-            i++;
+        int n = nums.size();
+        vector<int> res(n);
+
+        int l = 0, r = n - 1;
+        int idx = n - 1;
+
+        while (l <= r) {
+            if (abs(nums[l]) > abs(nums[r])) {
+                res[idx--] = nums[l] * nums[l];
+                l++;
+            } else {
+                res[idx--] = nums[r] * nums[r];
+                r--;
+            }
         }
 
-        cout<<i<<" "<<j<<endl;
+        return res;
 
-        while(j>=0 or i<nums.size()){
-            cout<<i<<" "<<j<<endl;
-            if(j>=0 and i<nums.size()){
-                if(abs(nums[j])<=nums[i]){
-                    ans.push_back(nums[j]*nums[j]);
-                    j--;
-                }
-                else if(abs(nums[j])>nums[i]){
-                    ans.push_back(nums[i]*nums[i]);
-                    i++;
-                }
 
-            }
-            else if(i<nums.size())
-            {
-                 ans.push_back(nums[i]*nums[i]);
-                    i++;
-            }
-            else if (j>=0){
-                ans.push_back(nums[j]*nums[j]);
-                    j--;
-            }
-        } 
-        return ans;
     }
 };
