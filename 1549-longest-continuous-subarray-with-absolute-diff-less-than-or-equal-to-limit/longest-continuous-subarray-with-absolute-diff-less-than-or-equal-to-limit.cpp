182f2453a272f5d84c216e5141f1fcfd313411dc
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-05-06 00:33:43 +0530

    Time: 11 ms (95.68%) | Memory: 74 MB (93.64%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
-        deque<int> maxi;
-        deque<int> mini;
-
-        int l=0,r=0, ans=0;;
+        deque<int> maxi,mini;
+        int l=0,r=0;
+        int ans=1;
         while(r<nums.size()){
             while(!maxi.empty() and nums[maxi.back()]<=nums[r])
                 maxi.pop_back();
             
             while(!mini.empty() and nums[mini.back()]>=nums[r])
                 mini.pop_back();
-            
+
             maxi.push_back(r);
             mini.push_back(r);
-            while(l<=r and nums[maxi.front()]-nums[mini.front()]>limit){
+            while(l<=r and nums[maxi.front()]-nums[mini.front()]>limit)
+            {
                 if(maxi.front()==l)
                     maxi.pop_front();
+                
                 if(mini.front()==l)
                     mini.pop_front();
+                
                 l++;
+                
             }
-
-            cout<<l<<" "<<r<<endl;
-            ans=max(r-l+1,ans);
+            ans=max(ans,r-l+1);
             r++;
         }
         return ans;
