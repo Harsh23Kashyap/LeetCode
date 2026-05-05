47f904a28bde212d563c1e22f2a184fcbe7ed4a1
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-05-06 01:09:57 +0530

    Time: 6 ms (32.75%) | Memory: 19.2 MB (20.34%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,66 +1,47 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
         
-        unordered_map<int, vector<pair<int,int>>> u;
-
-        // cout << "Building graph...\n";
-        for(auto it:flights) {
-            u[it[0]].push_back({it[1],it[2]});
-            // cout << "Edge: " << it[0] << " -> " << it[1] << " cost: " << it[2] << "\n";
+        vector<vector<pair<int,int>>> adj(n);
+        for(auto &f : flights) {
+            adj[f[0]].push_back({f[1], f[2]});
         }
-        vector<vector<int>> vis(n, vector<int> (k+2,INT_MAX));
 
-priority_queue<
-    pair<int,pair<int,int>>, 
-    vector<pair<int,pair<int,int>>>, 
-    greater<>
-> pq;
-        pq.push({0,{0,src}});
-        // cout << "\nInitial push: cost=0, stops=0, city=0\n";
+        vector<vector<int>> visited(n, vector<int>(K + 2, INT_MAX));
 
-        int fc=INT_MAX;
+        priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<>> pq;
 
-        while(!pq.empty()){
-            auto [cost,rem]=pq.top();
-            auto [ck,city]=rem;
+        pq.push({0, 0, src});
+        visited[src][0] = 0;
+
+        int ans = INT_MAX;
+
+        while(!pq.empty()) 
+        {
+            auto [cost, stops, node] = pq.top();
             pq.pop();
 
-            // cout << "\nPopped -> cost: " << cost << ", stops: " << ck << ", city: " << city << "\n";
-
-            if(vis[city][ck]<cost) {
-                // cout << "Skipping due to visited better state\n";
-                continue;
-            }
-
-            if(city==dst and ck-1<=k)
+            if(stops > K + 1) 
+                continue; 
+            if(node == dst) 
             {
-                // cout << "Reached destination " << dst << " with cost: " << cost << " and stops: " << ck << "\n";
-                fc=min(fc,cost);
-                continue;
+                ans = cost;
+                return ans;
             }
 
-            if(ck>k) {
-                // cout << "Skipping as stops exceeded: " << ck << "\n";
+            if(cost > visited[node][stops]) 
                 continue;
-            }
 
-            for(auto it:u[city]){
-                int ncity=it.first;
-                int ncost=it.second;
-
-                // cout << "  Exploring -> next city: " << ncity << ", edge cost: " << ncost << "\n";
-
-                if(ncost+cost<vis[ncity][ck+1])
+            for(auto &[nei, wt] : adj[node]) 
+            {
+                if(stops + 1 <= K + 1 and cost + wt < visited[nei][stops + 1]) 
                 {
-                    vis[ncity][ck+1]=ncost+cost; 
-                    pq.push({ncost+cost,{ck+1,ncity}}); 
-                }  
+                    visited[nei][stops + 1] = cost + wt;
+                    pq.push({cost + wt, stops + 1, nei});
+                }
             }
-
         }
 
-        cout << "\nFinal cheapest cost: " << fc << "\n";
-        return fc==INT_MAX?-1:fc;
+        return -1;
     }
 };
