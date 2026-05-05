941809ece1369c51379bfd43e11051b59469c045
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-05-05 20:26:32 +0530

    Time: 3 ms (22.61%) | Memory: 9.8 MB (11.65%) - LeetSync

--- 564-find-the-closest-palindrome/find-the-closest-palindrome.cpp
+++ 564-find-the-closest-palindrome/find-the-closest-palindrome.cpp
@@ -4,41 +4,68 @@ public:
         if(n.size()==1){
             if(n=="0")
                 return "1";
-            return to_string(n[0]-'0'-1);
+            
+            return to_string(stoi(n)-1);
         }
-        long long num = stoll(n);
-        int len = n.size();
-
-        // Edge candidates
-        vector<long long> candidates;
-        candidates.push_back(stoll(string(len - 1, '9')));
-        candidates.push_back(stoll(string(len , '9')));
-        candidates.push_back(stoll("1" + string(len - 1, '0') + "1"));
-        candidates.push_back(stoll("1" + string(len - 2, '0') + "1"));
- 
-        long long prefix = stoll(n.substr(0, (len + 1) / 2));
- 
-        for (long long i = prefix - 1; i <= prefix + 1; i++) {
-            string left=to_string(i);
-            string right=left;
-            if(len%2!=0)
-            right.pop_back();
-            reverse(right.begin(),right.end());
-            string curr=left+right;
-            candidates.push_back(stoll(curr));
-        }
-
-        long long best = -1;
-
-        for (auto cand : candidates) {
-            if (cand == num) continue;
-
-            if (best == -1 or llabs(cand - num) < llabs(best - num) or 
-                (llabs(cand - num) == llabs(best - num) and cand < best)) {
-                best = cand;
+        int len=n.size();
+        bool even=(len%2==0);
+        long long real=stoll(n);
+        vector<string> aa;
+        vector<string> options;
+        string fhh=n.substr(0,(len+1)/2);
+        string n99(n.size(),'9');
+        string n9(n.size()-1,'9');
+        string n10(n.size()-1,'0');
+        string n100(n.size()-2,'0');
+        long long fh=stoll(fhh);
+        aa.push_back(to_string(fh));
+        aa.push_back(to_string(fh+1));
+        aa.push_back(to_string(fh-1));
+        options.push_back(n99);
+        options.push_back(n9);
+        options.push_back("1"+n10+"1");
+         options.push_back("1"+n100+"1");
+        for(int i=0;i<aa.size();i++){
+           
+            string currres=aa[i];
+            string rev="";
+            if(even){
+                  rev=currres;
+                reverse(rev.begin(),rev.end());
             }
+            else{
+                  rev=currres;
+                rev.pop_back();
+                reverse(rev.begin(),rev.end());
+            }
+            options.push_back(currres+rev);
         }
+        
+        long long res=LONG_LONG_MAX;
+        for(int i=0;i<options.size();i++){ 
+            if(options[i]==n)
+            continue;
+            string currres=options[i];
+            cout<<options[i]<<endl;
+            long long curr=stoll(currres);
+            
+            if(abs(curr-real)<abs(res-real)){
+                res=curr;
 
-        return to_string(best);
+            }
+            else if(abs(curr-real)==abs(real-res)){
+                // cout<<" here "<<" "<<curr<<" "<<res<<endl;;
+                if(curr<res)
+                    res=curr;
+            }
+            // cout<<res<<endl;
+
+        }
+        return to_string(res);
+
+
+
+
+        
     }
 };
