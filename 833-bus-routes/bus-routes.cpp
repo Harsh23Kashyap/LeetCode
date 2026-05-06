4acce3c3d17cdf144e001ab04db4c6688628398a
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-05-06 23:24:52 +0530

    Time: 119 ms (85.58%) | Memory: 71.6 MB (58.56%) - LeetSync

--- 833-bus-routes/bus-routes.cpp
+++ 833-bus-routes/bus-routes.cpp
@@ -1,79 +1,70 @@
 class Solution {
 public:
     int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
-        if(source==target)
-        return 0;
-        unordered_set<int> visBus;
-        unordered_set<int> visStops;
 
-        unordered_map<int,vector<int>> bm;
+        if(source == target)
+            return 0;
 
-        // cout << "Building stop -> buses map\n";
-        for(int i=0;i<routes.size();i++){
-            for(int j=0;j<routes[i].size();j++){
-                bm[routes[i][j]].push_back(i);
-                // cout << "Stop " << routes[i][j] << " has bus " << i << "\n";
+        unordered_map<int, vector<int>> stopToBus;
+ 
+        for(int i = 0; i < routes.size(); i++) {
+            for(int stop : routes[i]) {
+                stopToBus[stop].push_back(i);
             }
         }
 
         queue<int> q;
 
-        // cout << "\nInitializing BFS from source: " << source << "\n";
-        for(int i:bm[source])
-        {
-            visBus.insert(i);
-            visStops.insert(source);
-            q.push(i);
-            // cout << "Initial bus added: " << i << "\n";
+        unordered_set<int> visBus;
+        unordered_set<int> visStops;
+ 
+        for(int bus : stopToBus[source]) {
+            q.push(bus);
+            visBus.insert(bus);
         }
 
-        int c=1;
+        visStops.insert(source);
 
-        while(!q.empty()){
-            int sz=q.size();
-            // cout << "\n--- LEVEL " << c << " ---\n";
-            // cout << "Queue size: " << sz << "\n";
+        int busesTaken = 1;
 
-            while(sz--){
-                auto it=q.front();
+        while(!q.empty()) 
+        {
+
+            int sz = q.size();
+            while(sz--) {
+                
+
+                int bus = q.front();
                 q.pop();
 
-                // cout << "\nProcessing Bus: " << it << "\n";
+                // explore all stops of this bus
+                for(int stop : routes[bus]) {
 
-                for(auto stops:routes[it]){
-                    // cout << "  Visiting stop: " << stops << "\n";
+                    // reached target
+                    if(stop == target)
+                        return busesTaken;
 
-                    if(stops==target){
-                        // cout << "🎯 Found target at stop " << stops << "\n";
-                        return c;
-                    }
+                    // already processed this stop
+                    if(visStops.count(stop))
+                        continue;
 
-                    if(visStops.find(stops)==visStops.end()){
-                        // cout << "  New stop discovered: " << stops << "\n";
+                    visStops.insert(stop);
 
-                        visStops.insert(stops);
+                    // from this stop, what buses can we take?
+                    for(int nextBus : stopToBus[stop]) {
 
-                        for(auto allbus:bm[stops]){
-                            // cout << "    Checking bus " << allbus << " at stop " << stops << "\n";
+                        if(visBus.count(nextBus))
+                            continue;
 
-                            if(visBus.find(allbus)==visBus.end()){
-                                // cout << "    ➕ Adding new bus: " << allbus << " to queue\n";
-
-                                q.push(allbus);
-                                visBus.insert(allbus);
-                            } else {
-                                // cout << "    ❌ Bus already visited: " << allbus << "\n";
-                            }
-                        }
-                    } else {
-                        // cout << "  ❌ Stop already visited: " << stops << "\n";
+                        visBus.insert(nextBus);
+                        q.push(nextBus);
                     }
                 }
             }
-            c++;
+
+            busesTaken++;
         }
 
-        // cout << "\nNo route found\n";
-        return -1;  
+        return -1;
     }
 };
