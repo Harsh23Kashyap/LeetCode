bc0743b92c27f97eef1ad6cca2db64049ac0a875
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-05-07 00:18:54 +0530

    Time: 103 ms (19.03%) | Memory: 148.7 MB (8.73%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -1,54 +1,27 @@
 class MedianFinder {
 public:
-priority_queue<int, vector<int>, greater<int>> right;
-priority_queue<int> left;
+    priority_queue<int> left; // max heap
+    priority_queue<int, vector<int>, greater<int>> right; // min heap
 
-    MedianFinder() {
-        
-    }
-    
+    MedianFinder() {}
+
     void addNum(int num) {
-        
-        if(left.empty() and right.empty())
         right.push(num);
 
-        else if (left.size()>right.size()){
-            left.push(num);
+        
+        left.push(right.top());
+        right.pop();
+
+        if (left.size() > right.size()) {
             right.push(left.top());
             left.pop();
         }
-        else if (right.size()>left.size()){
-            right.push(num);
-            left.push(right.top());
-            right.pop();
-        }
-
-        else{
-            left.push(num);
-            right.push(left.top());
-            left.pop();
-        }
-
-
-
     }
-    
+
     double findMedian() {
-        if(left.size()>right.size())
-            return left.top();
-        if(left.size()<right.size())
-            return right.top();
-
-        return (left.top()+right.top())/2.0;
-        
-
-        
+        if (left.size() == right.size()) {
+            return (left.top() + right.top()) / 2.0;
+        }
+        return right.top();
     }
 };
-
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder* obj = new MedianFinder();
- * obj->addNum(num);
- * double param_2 = obj->findMedian();
- */
