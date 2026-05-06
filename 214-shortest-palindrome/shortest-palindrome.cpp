8cdec1a27d5a2ee304221de13149d17af25167ae
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-05-06 15:11:58 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (96.40%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.cpp
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
- int mod=1e9+7;
- // We use rolling hash (Rabin–Karp) to find the longest palindromic prefix.
-// As we iterate, we maintain a forward hash and a reverse hash.
-// Whenever both hashes match, the substring [0..i] is a palindrome.
-// We track the last such index (longest palindromic prefix).
-// Then we take the remaining suffix, reverse it, and prepend it to the string
-// to form the shortest palindrome.
+ bool isPalindrome(string &s, int l, int r) {
+        while (l < r) {
+            if (s[l++] != s[r--]) return false;
+        }
+        return true;
+    }
     string shortestPalindrome(string s) {
         if(s.size()==0)
         return "";
-        long long rhash=0,lhash=0;
-         long long base=31;
-       
-         long long power=1;
-         long long last=0; 
-        string ans="";
+        long long base=31,power=1,lhash=0,rhash=0, mod=1e9+7;
+
+        int bestans=0;
         for(int i=0;i<s.size();i++){
-            lhash=(lhash*base+ (s[i]-'a'+1))%mod;
-            rhash=(rhash+ power*(s[i]-'a'+1))%mod;
-            power = (power * base) % mod;
-           
-            if(lhash==rhash){
-               last=i;
-            }
+            lhash=(lhash*base + (s[i]-'a'+1))%mod;
+            rhash=((s[i]-'a'+1)*power+rhash)%mod;
+            power=(power*base)%mod;
+            // if(lhash==rhash and isPalindrome(s,0,i))
+            if(lhash==rhash )
+                bestans=i;
         }
-        string rem=s.substr(last+1);
-        reverse(rem.begin(),rem.end());
-        return rem+s;
+
+        string curr=s.substr(bestans+1);
+        reverse(curr.begin(),curr.end());
+        return curr+s;
+
+
     }
 };
