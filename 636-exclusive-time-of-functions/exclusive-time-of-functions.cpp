b6bedc816806ab61f83342270d6386465e50a95e
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-05-07 21:28:12 +0530

    Time: 3 ms (73.18%) | Memory: 20.2 MB (15.25%) - LeetSync

--- 636-exclusive-time-of-functions/exclusive-time-of-functions.cpp
+++ 636-exclusive-time-of-functions/exclusive-time-of-functions.cpp
@@ -1,37 +1,78 @@
 class Solution {
 public:
+
     vector<int> exclusiveTime(int n, vector<string>& logs) {
+
         vector<int> ans(n, 0);
-        stack<int> st;
-        int prevTime = 0;
 
-        for (string log : logs) {
-            int id = 0, i = 0;
-             
-            while (log[i] != ':') {
-                id = id * 10 + (log[i] - '0');
-                i++;
-            }
-            i++; 
-            bool isStart = (log[i] == 's');  
-            i += isStart ? 6 : 4;  
- 
-            int time = 0;
-            while (i < log.size()) {
-                time = time * 10 + (log[i] - '0');
-                i++;
-            }
+        // {id, type, timestamp}
+        stack<tuple<int,string,int>> st;
 
-            if (isStart) {
-                if (!st.empty()) {
-                    ans[st.top()] += time - prevTime;
+        for(string log : logs) {
+
+            int first = log.find(':');
+            int second = log.find(':', first + 1);
+
+            int id =
+                stoi(log.substr(0, first));
+
+            string type =
+                log.substr(first + 1,
+                           second - first - 1);
+
+            int timestamp =
+                stoi(log.substr(second + 1));
+
+            // START
+            if(type == "start") {
+
+                // pause currently running process
+                if(!st.empty()) {
+
+                    auto [prevId, prevType, prevTime]
+                        = st.top();
+
+                    st.push({prevId, "end", timestamp});
                 }
-                st.push(id);
-                prevTime = time;
-            } else {
-                ans[st.top()] += time - prevTime + 1;
+
+                st.push({id, "start", timestamp});
+            }
+
+            // END
+            else {
+
+                auto [currId, currType, startTime]
+                    = st.top();
+
                 st.pop();
-                prevTime = time + 1;
+
+                // complete current process
+                ans[currId] +=
+                    timestamp - startTime + 1;
+
+                // process paused parent
+                if(!st.empty()) {
+
+                    auto [endId, endType, endTime]
+                        = st.top();
+
+                    st.pop();
+
+                    auto [startId, startType, startTime2]
+                        = st.top();
+
+                    st.pop();
+
+                    ans[startId] +=
+                        endTime - startTime2;
+
+                    // restart parent
+                    st.push({
+                        startId,
+                        "start",
+                        timestamp + 1
+                    });
+                }
             }
         }
 
