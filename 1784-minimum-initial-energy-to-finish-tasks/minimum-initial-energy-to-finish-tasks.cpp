b3c706bce8f7fbb5fb6de8e4d41fd47d17ddbe18
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-05-12 12:05:27 +0530

    Time: 96 ms (21.85%) | Memory: 110.5 MB (22.82%) - LeetSync

--- 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.cpp
+++ 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
 static bool cmp(vector<int>& a, vector<int>& b){
-    return (a[1]-a[0])>(b[1]-b[0]);
+    return (a[1]-a[0])<(b[1]-b[0]);
 }
- bool possible(vector<vector<int>> &tasks, int m){
-    for(auto it:tasks){
-        // cout<<it[0]<<" "<<it[1]<<" "<<m<<endl;;
-        if(it[1]>m)
-        return false;
-        m-=it[0];
-    }
-    return true;
-}
+//  bool possible(vector<vector<int>> &tasks, int m){
+//     for(auto it:tasks){
+//         // cout<<it[0]<<" "<<it[1]<<" "<<m<<endl;;
+//         if(it[1]>m)
+//         return false;
+//         m-=it[0];
+//     }
+//     return true;
+// }
     int minimumEffort(vector<vector<int>>& tasks) {
         sort(tasks.begin(),tasks.end(),cmp);
 
-        int l=tasks[0][1], h=1e9;
-        int ans=1e9;
-        while(l<=h){
-            int m=l+(h-l)/2;
-            cout<<m<<endl;
-            if(possible(tasks,m)){
-                // cout<<"works"<<endl;
-                ans=m;
-                h=m-1;
-            }
-            else
-            l=m+1;
+        // int l=tasks[0][1], h=1e9;
+        // int ans=1e9;
+        // while(l<=h){
+        //     int m=l+(h-l)/2;
+        //     cout<<m<<endl;
+        //     if(possible(tasks,m)){
+        //         // cout<<"works"<<endl;
+        //         ans=m;
+        //         h=m-1;
+        //     }
+        //     else
+        //     l=m+1;
+        // }
+        // return ans;
+        int ans=0;
+        for(auto it:tasks){
+            ans=max(ans+it[0],it[1]);
         }
         return ans;
     }
