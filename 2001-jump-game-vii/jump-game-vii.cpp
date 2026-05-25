73d9826bafe72861321c3b756b8ba47e51ddb5f7
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-05-24 21:15:45 -0700

    Time: 27 ms (9.47%) | Memory: 38.1 MB (6.45%) - LeetSync

--- 2001-jump-game-vii/jump-game-vii.cpp
+++ 2001-jump-game-vii/jump-game-vii.cpp
@@ -3,35 +3,34 @@ public:
     bool canReach(string s, int minJump, int maxJump) {
         vector<int> zero;
 
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '0') zero.push_back(i);
-        }
-
-        vector<int> dp(s.size(), -1);
-        dp[0] = 1;
-
-        int next = 1; // zero[0] is 0 already processed/known
-
-        for (int i = 0; i < zero.size(); i++) {
-            if (dp[zero[i]] == -1) continue;
-
-            int mini = zero[i] + minJump;
-            int maxi = min((int)s.size() - 1, zero[i] + maxJump);
-
-            int j = lower_bound(zero.begin() + next, zero.end(), mini) - zero.begin();
-
-            while (j < zero.size() && zero[j] <= maxi) {
-                dp[zero[j]] = 1;
-
-                if (zero[j] == s.size() - 1)
-                    return true;
-
-                j++;
+        for (int i = 0; i < s.size(); i++) 
+        {
+            if (s[i] == '0') {
+                zero.push_back(i);
             }
+        }
+        vector<int> dp(s.size(),-1);
+        dp[0]=1;
+        int next=1;
+        for (int i = 0; i < zero.size(); i++) {
+            if(dp[zero[i]]==-1)
+            continue;
+            int mini = minJump + zero[i];
+            int maxi = min((int)s.size() - 1, maxJump + zero[i]);
 
-            next = max(next, j); // skip already marked zeros forever
+            int j = lower_bound(zero.begin()+ next, zero.end(), mini) - zero.begin();
+
+            if (j == zero.size() or zero[j] > maxi)
+                continue;
+            int k = j;
+            for (; k < zero.size() and zero[k] <= maxi; k++) {
+                dp[zero[k]]=1;
+                if (zero[k] == s.size() - 1)
+                    return true;
+            }
+            next=max(next,k);
         }
 
-        return dp[s.size() - 1] == 1;
+        return false;
     }
 };
