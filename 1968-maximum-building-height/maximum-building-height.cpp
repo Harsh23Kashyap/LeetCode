2c613d6259c69542582f3079bb76a7aa91dfc097
Harsh Kashyap <harsh.kashyap2001@gmail.com>
2026-06-19 22:58:04 -0700

    Time: 51 ms (80.56%) | Memory: 106.5 MB (98.61%) - LeetSync

--- 1968-maximum-building-height/maximum-building-height.cpp
+++ 1968-maximum-building-height/maximum-building-height.cpp
@@ -2,22 +2,47 @@ class Solution {
 public:
     int maxBuilding(int n, vector<vector<int>>& restrictions) {
         restrictions.push_back({1, 0});
-        restrictions.push_back({n, n - 1});
+        bool found = false;
+        for (auto &r : restrictions) {
+            if (r[0] == n) 
+            {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+            restrictions.push_back({n, n - 1});
+
         sort(restrictions.begin(), restrictions.end());
 
         int m = restrictions.size();
-        for (int i = 1; i < m; ++i) {
-            restrictions[i][1] = min(restrictions[i][1], restrictions[i - 1][1] + restrictions[i][0] - restrictions[i - 1][0]);
+
+        for (int i = 1; i < m; i++) 
+        {
+            int dist = restrictions[i][0] - restrictions[i - 1][0];
+            restrictions[i][1] = min(restrictions[i][1],
+                                     restrictions[i - 1][1] + dist);
         }
-        for (int i = m - 2; i >= 0; --i) {
-            restrictions[i][1] = min(restrictions[i][1], restrictions[i + 1][1] + restrictions[i + 1][0] - restrictions[i][0]);
+
+        for (int i = m - 2; i >= 0; i--) {
+            int dist = restrictions[i + 1][0] - restrictions[i][0];
+            restrictions[i][1] = min(restrictions[i][1],
+                                     restrictions[i + 1][1] + dist);
         }
 
         int ans = 0;
-        for (int i = 1; i < m; ++i) {
-            int l = restrictions[i - 1][0], r = restrictions[i][0];
-            int hl = restrictions[i - 1][1], hr = restrictions[i][1];
-            ans = max(ans, (r - l + hl + hr) / 2);
+
+        for (int i = 1; i < m; i++) 
+        {
+            int x1 = restrictions[i - 1][0];
+            int h1 = restrictions[i - 1][1];
+
+            int x2 = restrictions[i][0];
+            int h2 = restrictions[i][1];
+
+            int dist = x2 - x1;
+
+            ans = max(ans, (h1 + h2 + dist) / 2);
         }
 
         return ans;
